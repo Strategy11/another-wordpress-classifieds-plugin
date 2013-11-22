2c3fcc0e4da830c21373721c5be841e5e15dffad
Willington Vega <wvega@wvega.com>
2013-11-22 16:06:19 -0500

    Update Comments & Ratings module version (#629)

--- awpcp.php
+++ awpcp.php
@@ -409,7 +409,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/comments-ratings-module/?ref=user-panel',
 					'installed' => defined( 'AWPCP_COMMENTS_MODULE' ),
 					'version' => 'AWPCP_COMMENTS_MODULE_VERSION',
-					'required' => '3.0.1',
+					'required' => '3.0.2',
 				),
 				'coupons' => array(
 					'name' => __( 'Coupons/Discount', 'AWPCP' ),
