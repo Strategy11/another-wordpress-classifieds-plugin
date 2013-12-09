6c5f769f072507707a6d14aeea41ceb5f215cd92
Willington Vega <wvega@wvega.com>
2013-12-09 13:44:46 -0500

    Update Multiple Region Selector CSS style (#645)

--- frontend/templates/html-widget-multiple-region-selector.tpl.php
+++ frontend/templates/html-widget-multiple-region-selector.tpl.php
@@ -2,14 +2,14 @@
 
     <ul data-bind="foreach: regions">
         <li class="awpcp-region-selector">
-            <ul data-bind="foreach: partials">
+            <ul class="awpcp-region-selector-partials" data-bind="foreach: partials">
                 <li class="awpcp-region-selector-partial" data-bind="visible: visible">
                     <label data-bind="attr: { 'for': id }, text: label"></label>
 
                     <select class="multiple-region" data-bind="attr: { id: id, name: selectName }, options: options, optionsText: 'name', optionsValue: 'id', optionsCaption: caption, value: selectedOption, visible: showSelectField, disable: $root.options.disabled">
                     </select>
 
-                    <input class="multiple-region" type="text" data-bind="attr: { id: id, name: textfieldName }, value: selectedText, visible: showTextField, disable: $root.options.disabled" />
+                    <input class="multiple-region inputbox" type="text" data-bind="attr: { id: id, name: textfieldName }, value: selectedText, visible: showTextField, disable: $root.options.disabled" />
 
                     <span class="loading-message" data-bind="visible: loading"><?php echo _x( 'loading...', 'loading region options', 'AWPCP' ); ?></span>
 
