186e3e5e3eeb3cda9087e160d8de2d65cd0225d9
Willington Vega <wvega@wvega.com>
2014-01-09 10:38:50 -0600

    Fix hardcoded database table name.

--- includes/views/admin/listings/class-listings-table-search-by-user-condition.php
+++ includes/views/admin/listings/class-listings-table-search-by-user-condition.php
@@ -9,11 +9,11 @@ class AWPCP_ListingsTableSearchByUserCondition {
     public function create( $search_term ) {
         global $wpdb;
 
-        $sql = "SELECT DISTINCT ID FROM wp_users ";
-        $sql.= "LEFT JOIN wp_usermeta ON (ID = user_id) ";
-        $sql.= 'WHERE (user_login LIKE \'%%%1$s%%\') OR ';
-        $sql.= '(meta_key = \'last_name\' AND meta_value LIKE \'%%%1$s%%\') ';
-        $sql.= 'OR (meta_key = \'first_name\' AND meta_value LIKE \'%%%1$s%%\')';
+        $sql = "SELECT DISTINCT ID FROM " . $wpdb->users . " ";
+        $sql.= "LEFT JOIN " . $wpdb->usermeta . " ON (ID = user_id) ";
+        $sql.= 'WHERE (user_login LIKE \'%%%1$s%%\') ';
+        $sql.= 'OR (meta_key = \'last_name\' AND meta_value LIKE \'%%%1$s%%\') ';
+        $sql.= 'OR (meta_key = \'first_name\' AND meta_value LIKE \'%%%1$s%%\') ';
 
         $users = $wpdb->get_col( $wpdb->prepare( $sql, $search_term ) );
 
