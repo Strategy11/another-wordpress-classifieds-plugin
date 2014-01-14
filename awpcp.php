3a064eabfb646ebe7ddd6b2211abc5772474b590
Willington Vega <wvega@wvega.com>
2014-01-14 12:32:59 -0600

    Bump Region Control module version to 3.2.1.

--- awpcp.php
+++ awpcp.php
@@ -500,7 +500,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/regions-control-module?ref=panel',
 					'installed' => $hasregionsmodule,
 					'version' => 'AWPCP_REGION_CONTROL_MODULE_DB_VERSION',
-					'required' => '3.2',
+					'required' => '3.2.1',
 				),
 				'rss' => array(
 					'name' => __( 'RSS', 'AWPCP' ),
