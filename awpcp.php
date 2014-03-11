7cba4c6d319b56013fe236ddf029ee6f3bb419a8
Willington Vega <wvega@wvega.com>
2014-03-10 21:44:44 -0500

    Bump version of Category Icon module to 3.2.1

--- awpcp.php
+++ awpcp.php
@@ -460,7 +460,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/category-icons-module?ref=panel',
 					'installed' => $hascaticonsmodule,
 					'version' => 'AWPCP_CATEGORY_ICONS_MODULE_DB_VERSION',
-					'required' => '3.0.2',
+					'required' => '3.2.1',
 				),
 				'comments' => array(
 					'name' => __(  'Comments & Ratings', 'AWPCP'  ),
