a45d4a6e6c17e0d866861a51b7db9888c1407299
Willington Vega <wvega@wvega.com>
2014-03-19 13:59:01 -0500

    Bump Extra Fields version to 3.2.6.

--- awpcp.php
+++ awpcp.php
@@ -444,7 +444,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/extra-fields-module?ref=panel',
 					'installed' => defined( 'AWPCP_EXTRA_FIELDS_MODULE' ),
 					'version' => 'AWPCP_EXTRA_FIELDS_MODULE_DB_VERSION',
-					'required' => '3.2.5',
+					'required' => '3.2.6',
 				),
 				'featured-ads' => array(
 					'name' => __( 'Featured Ads', 'AWPCP' ),
