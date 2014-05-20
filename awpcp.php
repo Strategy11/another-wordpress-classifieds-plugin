11a4ffc6a35e57ce5b54f122991e9db581af2bc9
Willington Vega <wvega@wvega.com>
2014-05-20 17:25:49 -0500

    Subscriptions 3.2.8.

--- awpcp.php
+++ awpcp.php
@@ -522,7 +522,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/subscriptions-module/?ref=panel',
 					'installed' => defined( 'AWPCP_SUBSCRIPTIONS_MODULE' ),
 					'version' => 'AWPCP_SUBSCRIPTIONS_MODULE_DB_VERSION',
-					'required' => '3.2.7',
+					'required' => '3.2.8',
 				),
 				'xml-sitemap' => array(
 					'name' => __(  'XML Sitemap', 'AWPCP'  ),
