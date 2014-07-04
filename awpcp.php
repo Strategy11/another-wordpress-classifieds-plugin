34d8b14fe995eb58daeac9c9cc7b429eeaa06f29
Willington Vega <wvega@wvega.com>
2014-07-04 10:30:07 -0500

    Region Control 3.2.17.

--- awpcp.php
+++ awpcp.php
@@ -550,7 +550,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/regions-control-module?ref=panel',
 					'installed' => defined( 'AWPCP_REGION_CONTROL_MODULE' ),
 					'version' => 'AWPCP_REGION_CONTROL_MODULE_DB_VERSION',
-					'required' => '3.2.16',
+					'required' => '3.2.17',
 				),
 				'rss' => array(
 					'name' => __( 'RSS', 'AWPCP' ),
