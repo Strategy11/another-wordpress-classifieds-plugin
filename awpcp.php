dc90acb103fd9a577bd80c263d0aba7b7663f3eb
Willington Vega <wvega@wvega.com>
2014-07-20 09:42:39 -0500

    Attachments 3.2.4.

--- awpcp.php
+++ awpcp.php
@@ -477,7 +477,7 @@ class AWPCP {
 					'url' => '',
 					'installed' => defined( 'AWPCP_ATTACHMENTS_MODULE' ),
 					'version' => 'AWPCP_ATTACHMENTS_MODULE_DB_VERSION',
-					'required' => '3.2.3',
+					'required' => '3.2.4',
 				),
 				'authorize.net' => array(
 					'name' => __(  'Authorize.Net', 'AWPCP'  ),
