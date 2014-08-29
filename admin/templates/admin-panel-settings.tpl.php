088a4fb41ca45390dd71e8673a7192834479d4da
Willington Vega <wvega@wvega.com>
2014-08-29 00:44:37 -0500

    Fix PHP Warning in Settings screen.

--- admin/templates/admin-panel-settings.tpl.php
+++ admin/templates/admin-panel-settings.tpl.php
@@ -17,7 +17,7 @@
 			<?php do_action('awpcp-admin-settings-page--' . $group->slug); ?>
 
 			<form class="settings-form" action="<?php echo admin_url('options.php') ?>" method="post">
-				<?php settings_fields($awpcp->settings->option); ?>
+				<?php settings_fields( $awpcp->settings->setting_name ); ?>
 				<input type="hidden" name="group" value="<?php echo $group->slug ?>" />
 
 				<?php $awpcp->settings->load() ?>
