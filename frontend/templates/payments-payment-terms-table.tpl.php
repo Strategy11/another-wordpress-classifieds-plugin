8d1ef23b04f10cd3d55470cc96f2ac056bfc9c15
Willington Vega <wvega@wvega.com>
2014-09-06 13:41:49 -0500

    Do not scape item_column's output; the content may be html that needs to be rendered raw.

--- frontend/templates/payments-payment-terms-table.tpl.php
+++ frontend/templates/payments-payment-terms-table.tpl.php
@@ -20,7 +20,7 @@
 
         <tr <?php echo $this->item_attributes($item) ?>>
             <?php foreach ($columns as $column => $name): ?>
-            <td data-title="<?php echo esc_attr( $name ); ?>"><?php echo esc_html( $this->item_column( $item, $column ) ); ?></td>
+            <td data-title="<?php echo esc_attr( $name ); ?>"><?php /* item_column() returns escaped strings */ echo $this->item_column( $item, $column ); ?></td>
             <?php endforeach ?>
         </tr>
 
