343cd8195730df63483a40e87de4c3e1d65a207e
Willington Vega <wvega@wvega.com>
2014-10-12 09:43:01 -0500

    Improve appearance of Videos and other files in Media Manager Component.

--- templates/components/media-manager.tpl.php
+++ templates/components/media-manager.tpl.php
@@ -1,6 +1,6 @@
 <div class="awpcp-media-manager">
     <div class="awpcp-uploaded-images" data-bind="if: haveImages">
-        <h3><?php echo esc_html( __( 'Images', 'AWPCP' ) ); ?></h3>
+        <h3 class="awpcp-uploaded-files-group-title"><?php echo esc_html( __( 'Images', 'AWPCP' ) ); ?></h3>
         <ul class="awpcp-uploaded-files-list clearfix" data-bind="foreach: { data: images, as: 'image' }">
             <li data-bind="css: $root.getFileCSSClasses( image ), attr: { id: $root.getFileId( image ) }">
                 <div class="awpcp-uploaded-file-thumbnail-container">
@@ -26,28 +26,47 @@
         </ul>
     </div>
 
-    <div class="awpcp-uploaded-images" data-bind="if: haveVideos">
-        <h3><?php echo esc_html( __( 'Videos', 'AWPCP' ) ); ?></h3>
-        <ul>
-            <li>
-                <img>
-                <ul>
-                    <li><a href="#"></a></li>
+    <div class="awpcp-uploaded-videos" data-bind="if: haveVideos">
+        <h3 class="awpcp-uploaded-files-group-title"><?php echo esc_html( __( 'Videos', 'AWPCP' ) ); ?></h3>
+        <ul class="awpcp-uploaded-files-list clearfix" data-bind="foreach: { data: videos, as: 'video' }">
+            <li data-bind="css: $root.getFileCSSClasses( video ), attr: { id: $root.getFileId( video ) }">
+                <div class="awpcp-uploaded-file-thumbnail-container">
+                    <img data-bind="attr: { src: thumbnailUrl }">
+                </div>
+                <ul class="awpcp-uploaded-file-actions clearfix">
+                    <li class="awpcp-uploaded-file-action awpcp-uploaded-file-change-status-action">
+                        <label>
+                            <input type="checkbox" data-bind="checked: enabled"> <?php echo esc_html( __( 'Enabled', 'AWPCP' ) ); ?>
+                        </label>
+                    </li>
+                    <li class="awpcp-uploaded-video-delete-action awpcp-uploaded-file-delete-action awpcp-uploaded-file-action"><a title="<?php echo esc_attr( __( 'Delete Image', 'AWPCP' ) ); ?>" data-bind="click: $root.deleteFile"></a></li>
+                    <li class="awpcp-uploaded-file-action awpcp-uploaded-file-loading-icon" data-bind="visible: isBeingModified"><span class="awpcp-spinner awpcp-spinner-visible"></span></li>
                 </ul>
+                <div class="awpcp-uploaded-file-primary-label" data-bind="visible: isPrimary"><?php echo esc_html(  __( 'Primary Image', 'AWPCP' ) ); ?></div>
             </li>
         </ul>
     </div>
 
-
-    <div class="awpcp-uploaded-images" data-bind="if: haveOtherFiles">
-        <h3><?php echo esc_html( __( 'Other Files', 'AWPCP' ) ); ?></h3>
-        <table>
-            <tbody>
-                <tr>
-                    <td></td>
+    <div class="awpcp-uploaded-files" data-bind="if: haveOtherFiles">
+        <h3 class="awpcp-uploaded-files-group-title"><?php echo esc_html( __( 'Other Files', 'AWPCP' ) ); ?></h3>
+        <table class="awpcp-uploaded-files-table">
+            <tbody data-bind="foreach: { data: others, as: 'file' }">
+                <tr data-bind="css: $root.getFileCSSClasses( file ), attr: { id: $root.getFileId( file ) }">
+                    <td class="awpcp-uploaded-file-name">
+                        <a target="_blank">
+                            <img data-bind="attr: { src: iconUrl }" />
+                            <span data-bind="text: name"></span>
+                        </a>
+                    </td>
                     <td>
-                        <ul>
-                            <li><a href="#"></a></li>
+                        <ul class="awpcp-uploaded-file-actions clearfix">
+                            <li class="awpcp-uploaded-file-action awpcp-uploaded-file-change-status-action">
+                                <label>
+                                    <input type="checkbox" data-bind="checked: enabled"> <?php echo esc_html( __( 'Enabled', 'AWPCP' ) ); ?>
+                                </label>
+                            </li>
+                            <li class="awpcp-uploaded-file-action awpcp-uploaded-file-delete-action"><a title="<?php echo esc_attr( __( 'Delete Image', 'AWPCP' ) ); ?>" data-bind="click: $root.deleteFile"></a></li>
+                            <li class="awpcp-uploaded-file-action awpcp-uploaded-file-loading-icon" data-bind="visible: isBeingModified"><span class="awpcp-spinner awpcp-spinner-visible"></span></li>
                         </ul>
                     </td>
                 </tr>
