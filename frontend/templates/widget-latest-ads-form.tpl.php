948a343169fc8ba09f30464f6b40d84593fd32da
Willington Vega <wvega@wvega.com>
2014-10-16 18:42:14 -0500

    Add a point to the end of the checkbox labels in the Latest Ads Widget form.

--- frontend/templates/widget-latest-ads-form.tpl.php
+++ frontend/templates/widget-latest-ads-form.tpl.php
@@ -11,17 +11,17 @@
 <p>
     <input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'show-title' ) ); ?>" value="0" />
     <input type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show-title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show-title' ) ); ?>" value="1" <?php echo $instance['show-title'] ? 'checked="true"' : ''; ?> />
-    <label for="<?php echo esc_attr( $this->get_field_id( 'show-title' ) ); ?>"><?php _e('Show Ad title', 'AWPCP'); ?></label>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'show-title' ) ); ?>"><?php _e('Show Ad title.', 'AWPCP'); ?></label>
     <br/>
     <input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'show-excerpt' ) ); ?>" value="0" />
     <input type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show-excerpt' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show-excerpt' ) ); ?>" value="1" <?php echo $instance['show-excerpt'] ? 'checked="true"' : ''; ?> />
-    <label for="<?php echo esc_attr( $this->get_field_id( 'show-excerpt' ) ); ?>"><?php _e('Show Ad excerpt', 'AWPCP'); ?></label>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'show-excerpt' ) ); ?>"><?php _e('Show Ad excerpt.', 'AWPCP'); ?></label>
     <br/>
     <input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'show-images' ) ); ?>" value="0" />
     <input type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show-images' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show-images' ) ); ?>" value="1" <?php echo $instance['show-images'] ? 'checked="true"' : ''; ?> />
-    <label for="<?php echo esc_attr( $this->get_field_id( 'show-images' ) ); ?>"><?php _e('Show Thumbnails in Widget', 'AWPCP'); ?></label>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'show-images' ) ); ?>"><?php _e('Show thumbnails in widget.', 'AWPCP'); ?></label>
     <br/>
     <input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'show-blank' ) ); ?>" value="0" />
     <input type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show-blank' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show-blank' ) ); ?>" value="1" <?php echo $instance['show-blank'] ? 'checked="true"' : ''; ?>/>
-    <label for="<?php echo esc_attr( $this->get_field_id( 'show-blank' ) ); ?>"><?php _e('Show "No Image" PNG when Ad has no picture. Improves layout.', 'AWPCP'); ?></label>
+    <label for="<?php echo esc_attr( $this->get_field_id( 'show-blank' ) ); ?>"><?php _e('Show "No Image" PNG when Ad has no picture (improves layout).', 'AWPCP'); ?></label>
 </p>
