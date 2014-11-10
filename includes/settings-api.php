936b17771865f96a8778617f6571f0fd81f94db5
Willington Vega <wvega@wvega.com>
2014-11-09 20:49:14 -0500

    Always show help text for textfield settings below the field.

--- includes/settings-api.php
+++ includes/settings-api.php
@@ -1012,8 +1012,7 @@ class AWPCP_Settings_API {
 
 		$html = '<input id="'. $setting->name . '" class="regular-text" ';
 		$html.= 'value="' . $value . '" type="' . $type . '" ';
-		$html.= 'name="awpcp-options[' . $setting->name . ']" />';
-		$html.= strlen($setting->helptext) > 60 ? '<br/>' : '';
+		$html.= 'name="awpcp-options[' . $setting->name . ']" /><br/>';
 		$html.= '<span class="description">' . $setting->helptext . '</span>';
 
 		echo $html;
