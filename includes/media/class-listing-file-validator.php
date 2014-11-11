6a82e2275d8bfd52e2cd773ca07465fa0ddc7dd2
Willington Vega <wvega@wvega.com>
2014-11-11 17:20:27 -0500

    Remove image validation code from Listing File Validator abstract class.

--- includes/media/class-listing-file-validator.php
+++ includes/media/class-listing-file-validator.php
@@ -59,28 +59,6 @@ abstract class AWPCP_ListingFileValidator {
         }
     }
 
-    private function validate_image_dimensions( $file, $upload_limits ) {
-        $img_info = getimagesize( $file->get_path() );
-
-        if ( ! isset( $img_info[ 0 ] ) && ! isset( $img_info[ 1 ] ) ) {
-            $message = _x( 'There was an error trying to find out the dimension of <filename>. The file was not uploaded.', 'upload files', 'AWPCP' );
-            $message = str_replace( '<filename>', '<strong>' . $file->get_real_name() . '</strong>' );
-            throw new AWPCP_Exception( $message );
-        }
-
-        if ( $img_info[ 0 ] < $upload_limits['min_image_width'] ) {
-            $message = _x( 'The image %s did not meet the minimum width of %s pixels. The file was not uploaded.', 'upload files', 'AWPCP');
-            $message = sprintf( $message, '<strong>' . $file->get_real_name() . '</strong>', $upload_limits['min_image_width'] );
-            throw new AWPCP_Exception( $message );
-        }
-
-        if ( $img_info[ 1 ] < $upload_limits['min_image_height'] ) {
-            $message = _x( 'The image %s did not meet the minimum height of %s pixels. The file was not uploaded.', 'upload files', 'AWPCP');
-            $message = sprintf( $message, '<strong>' . $file->get_real_name() . '</strong>', $upload_limits['min_image_height'] );
-            throw new AWPCP_Exception( $message );
-        }
-    }
-
     protected function additional_verifications( $file, $upload_limits ) {
         // nothing here!
     }
