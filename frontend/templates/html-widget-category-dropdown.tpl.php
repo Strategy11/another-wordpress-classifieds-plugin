fa268f22a515dca1bc65813c6fde670f59d2d6bf
Willington Vega <wvega@wvega.com>
2014-11-13 21:22:08 -0500

    Fix PHP Notice in Category Dropdown widget.

--- frontend/templates/html-widget-category-dropdown.tpl.php
+++ frontend/templates/html-widget-category-dropdown.tpl.php
@@ -5,6 +5,7 @@
 <?php $hash = uniqid(); ?>
 
 <?php if ( $use_multiple_dropdowns ): ?>
+
 <input id="awpcp-category-dropdown-<?php echo $hash; ?>" type="hidden" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $selected ); ?>" />
 <select class="awpcp-category-dropdown awpcp-dropdown <?php echo $required ? 'required' : ''; ?>" id="awpcp-category-dropdown" target="<?php echo esc_attr( $hash ); ?>" chain="<?php echo esc_attr( join( ',', $chain ) ); ?>" next-default-option="<?php echo esc_attr( $placeholders['default-option-second-level'] ); ?>">
     <option class="default" value=""><?php echo esc_html( $placeholders['default-option-first-level'] ); ?></option>
