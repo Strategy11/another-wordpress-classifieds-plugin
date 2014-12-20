965b5c2500ee35e71f249dab11ca4f52fd3aecbc
Willington Vega <wvega@wvega.com>
2014-12-20 17:07:19 -0500

    Allow modules to define file types.

--- includes/media/class-file-types.php
+++ includes/media/class-file-types.php
@@ -1,25 +1,25 @@
 <?php
 
 function awpcp_file_types() {
-    return new AWPCP_FileTypes( awpcp()->settings );
-}
+    static $instance = null;
 
-/**
- * TODO: allow modules to register file types.
- */
-class AWPCP_FileTypes {
-
-    private $file_types;
-
-    private $settings;
-
-    public function __construct( $settings ) {
-        $this->settings = $settings;
-        $this->define_file_types();
+    if ( is_null( $instance ) ) {
+        $instance = awpcp_file_types_builder()->create_instance();
     }
 
-    private function define_file_types() {
-        $this->image_file_types = array(
+    return $instance;
+}
+
+function awpcp_file_types_builder() {
+    return new AWPCP_FileTypesBuilder;
+}
+
+class AWPCP_FileTypesBuilder {
+
+    public function create_instance() {
+        $instance = new AWPCP_FileTypes( awpcp()->settings );
+
+        $instance->add_file_types( 'image', array(
             'png' => array(
                 'name' => 'PNG',
                 'mime_types' => array( 'image/png' ),
@@ -32,67 +32,30 @@ class AWPCP_FileTypes {
                 'name' => 'GIF',
                 'mime_types' => array( 'image/gif' ),
             ),
-        );
+        ) );
 
-        $this->video_file_types = array(
-            'mov' => array(
-                'name' => 'MOV',
-                'mime_types' => array( 'video/quicktime' ),
-            ),
-            'mp4' => array(
-                'name' => 'MP4',
-                'mime_types' => array( 'video/mp4' ),
-            ),
-            'ogv' => array(
-                'name' => 'OGV',
-                'mime_types' => array(
-                    'video/ogg',
-                    'application/ogg'
-                ),
-            ),
-            'webm' => array(
-                'name' => 'WEBM',
-                'mime_types' => array(
-                    'video/webm',
-                    'application/octet-stream',
-                ),
-            ),
-            'avi' => array(
-                'name' => 'AVI',
-                'mime_types' => array(
-                    'video/avi',
-                    'video/x-msvideo',
-                ),
-            ),
-            'asf' => array(
-                'name' => 'ASF',
-                'mime_types' => array(
-                    'video/asf',
-                    'video/x-ms-asf',
-                ),
-            ),
-        );
+        do_action( 'awpcp-register-file-types', $instance );
 
-        $this->other_file_types = array(
-            'pdf' => array(
-                'name' => 'PDF',
-                'mime_types' => array( 'application/pdf', 'application/x-pdf', 'application/vnd.pdf' ),
-            ),
-            'rtf' => array(
-                'name' => 'RTF',
-                'mime_types' => array( 'application/rtf', 'application/x-rtf', 'text/richtext' ),
-            ),
-            'txt' => array(
-                'name' => 'TXT',
-                'mime_types' => array( 'text/plain' ),
-            ),
-        );
+        return $instance;
+    }
+}
 
-        $this->file_types = array_merge( $this->image_file_types, $this->video_file_types, $this->other_file_types );
+class AWPCP_FileTypes {
+
+    private $file_types = array();
+
+    private $settings;
+
+    public function __construct( $settings ) {
+        $this->settings = $settings;
+    }
+
+    public function add_file_types( $group, $file_types ) {
+        $this->file_types[ $group ] = $file_types;
     }
 
     public function get_video_mime_types() {
-        return $this->get_mime_types( $this->video_file_types );
+        return $this->get_mime_types( $this->file_types['video'] );
     }
 
     private function get_mime_types( $file_types ) {
@@ -108,7 +71,7 @@ class AWPCP_FileTypes {
     }
 
     public function get_allowed_video_mime_types() {
-        return $this->get_allowed_mime_types( $this->video_file_types, $this->get_allowed_video_extensions() );
+        return $this->get_allowed_mime_types( $this->file_types['video'], $this->get_allowed_video_extensions() );
     }
 
     private function get_allowed_mime_types( $file_types, $allowed_extensions ) {
@@ -124,7 +87,7 @@ class AWPCP_FileTypes {
     }
 
     public function get_video_extensions() {
-        return array_keys( $this->video_file_types );
+        return array_keys( $this->file_types['video'] );
     }
 
     public function get_allowed_video_extensions() {
@@ -132,15 +95,15 @@ class AWPCP_FileTypes {
     }
 
     public function get_other_files_mime_types() {
-        return $this->get_mime_types( $this->other_file_types );
+        return $this->get_mime_types( $this->file_types['others'] );
     }
 
     public function get_other_allowed_files_mime_types() {
-        return $this->get_allowed_mime_types( $this->other_file_types, $this->get_other_allowed_files_extensions() );
+        return $this->get_allowed_mime_types( $this->file_types['others'], $this->get_other_allowed_files_extensions() );
     }
 
     public function get_other_files_extensions() {
-        return array_keys( $this->other_file_types );
+        return array_keys( $this->file_types['others'] );
     }
 
     public function get_other_allowed_files_extensions() {
