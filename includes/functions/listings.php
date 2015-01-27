d1e63af019584f0210df7269f726053b6d89f259
Willington Vega <wvega@wvega.com>
2015-01-27 17:58:36 -0500

    Remove development comments from awpcp_display_listings function.

--- includes/functions/listings.php
+++ includes/functions/listings.php
@@ -16,8 +16,6 @@ function awpcp_display_listings( $query, $context, $options ) {
         'after_content' => '',
     ) );
 
-    // DONE?: see awpcp_browse_ads_template_action is being used and use the output of that function
-    //      as the return value for this function.
     if ( has_action( 'awpcp_browse_ads_template_action' ) || has_filter( 'awpcp_browse_ads_template_filter' ) ) {
         do_action( 'awpcp_browse_ads_template_action' );
         return apply_filters( 'awpcp_browse_ads_template_filter' );
@@ -39,24 +37,6 @@ function awpcp_display_listings( $query, $context, $options ) {
     $listings = $listings_collection->find_enabled_listings_with_query( $query );
     $listings_count = $listings_collection->count_enabled_listings_with_query( $query );
 
-    // debugp( $listings, $listings_count );
-
-    // DONE?: take a show_intro_message option.
-    // DONE?: take a show_menu_items option.
-    // DONE?: take a show_category_selector option.
-
-    // DONE?: take a before_content option and insert it into the output
-    // DONE?: use awpcp-listings-before-content or similar hook to show region selector depending on context:
-    //      - show in standard plugin pages, except Search Ads
-    //      - do not show in shortcodes and others?
-
-    // awpcp-content-before-listings-page => before the page's content, classiwrapper first children
-    // awpcp-listings-before-content => before listings pagination
-    // awpcp-display-ads-before-list => after pagination, before list of listings
-    // awpcp-listings-after-content => after listings pagination
-    // awpcp-content-after-listings-page => right at the end of the page, before clasiwrapper closing tag
-
-    // DONE?: apply filters for content before listings page and show that content.
     $before_content = apply_filters( 'awpcp-content-before-listings-page', $options['before_content'], $context );
 
     $before_pagination = array();
@@ -75,7 +55,6 @@ function awpcp_display_listings( $query, $context, $options ) {
     $before_list = apply_filters( 'awpcp-display-ads-before-list', $options['before_list'], $context );
 
     if ( $listings_count > 0 ) {
-        // DONE?: show pagination at the top
         $pagination_options = array(
             'results' => $results_per_page,
             'offset' => $results_offset,
@@ -88,10 +67,7 @@ function awpcp_display_listings( $query, $context, $options ) {
         $pagination = '';
         $items = array();
     }
-    // DONE?: show items list
-    // DONE?: show pagination at the bottom
 
-    // DONE?: apply filters for content after listings page and show that content.
     $after_pagination = array( 'user-content' => $options['after_pagination'] );
     $after_pagination = apply_filters( 'awpcp-listings-after-content', $after_pagination, $context );
 
