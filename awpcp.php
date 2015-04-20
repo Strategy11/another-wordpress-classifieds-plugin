4b8234fb7aa495c0ff078be5a173790478249889
Willington Vega <wvega@wvega.com>
2015-04-20 09:16:00 -0500

    Bump version number in premium modules.

--- awpcp.php
+++ awpcp.php
@@ -692,7 +692,7 @@ class AWPCP {
 					'url' => 'http://awpcp.com/premium-modules/attachments-module/?ref=panel',
 					'installed' => defined( 'AWPCP_ATTACHMENTS_MODULE' ),
 					'version' => 'AWPCP_ATTACHMENTS_MODULE_DB_VERSION',
-					'required' => '3.4.1',
+					'required' => '3.4.2',
 				),
 				'authorize.net' => array(
 					'name' => __(  'Authorize.Net', 'AWPCP'  ),
@@ -811,7 +811,7 @@ class AWPCP {
                     'url' => 'http://www.awpcp.com/',
                     'installed' => defined( 'AWPCP_VIDEOS_MODULE' ),
                     'version' => 'AWPCP_VIDEOS_MODULE_DB_VERSION',
-                    'required' => '1.0.2',
+                    'required' => '1.0.3',
                 ),
 				'xml-sitemap' => array(
 					'name' => __(  'XML Sitemap', 'AWPCP'  ),
