a9ff4ce097d020b793395d01e8b2afa4f4707282
Willington Vega <wvega@wvega.com>
2015-04-30 08:04:24 -0500

    Fix verification emails being sent when an administrator posted a listing (#1113).

--- includes/class-awpcp-listings-api.php
+++ includes/class-awpcp-listings-api.php
@@ -92,7 +92,7 @@ class AWPCP_ListingsAPI {
 
         if ( $ad->verified && ! awpcp_current_user_is_moderator() ) {
             $this->send_ad_posted_email_notifications( $ad, array(), $transaction );
-        } else {
+        } else if ( ! $ad->verified ) {
             $this->send_verification_email( $ad );
         }
 
