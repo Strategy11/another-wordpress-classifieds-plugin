20cfe72a33fe7d48772ed5f08521cf8acad1f963
Willington Vega <wvega@wvega.com>
2015-07-10 19:06:24 -0500

    Bump Coupons version to 3.5.2.

--- awpcp.php
+++ awpcp.php
@@ -752,7 +752,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/coupon-module/?ref=panel',
 					'installed' => defined( 'AWPCP_COUPONS_MODULE' ),
 					'version' => 'AWPCP_COUPONS_MODULE_DB_VERSION',
-					'required' => '3.0.3',
+					'required' => '3.5.2',
 				),
 				'extra-fields' => array(
 					'name' => __( 'Extra Fields', 'AWPCP' ),
