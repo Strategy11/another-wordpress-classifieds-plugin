54045933dd721cdbd12d63d43bd09935a5fbf5ad
Willington Vega <wvega@wvega.com>
2015-07-29 20:18:51 -0500

    Bump Attachments module version.

--- awpcp.php
+++ awpcp.php
@@ -719,7 +719,7 @@ class AWPCP {
 					'url' => 'http://awpcp.com/premium-modules/attachments-module/?ref=panel',
 					'installed' => defined( 'AWPCP_ATTACHMENTS_MODULE' ),
 					'version' => 'AWPCP_ATTACHMENTS_MODULE_DB_VERSION',
-					'required' => '3.5.1',
+					'required' => '3.5.2',
 				),
 				'authorize.net' => array(
 					'name' => __(  'Authorize.Net', 'AWPCP'  ),
