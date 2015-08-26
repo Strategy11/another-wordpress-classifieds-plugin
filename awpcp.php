ad41398e0d313471530e8f877b6587e0cb8afbd3
Willington Vega <wvega@wvega.com>
2015-08-26 09:21:24 -0500

    Update required version of Region Control module.

--- awpcp.php
+++ awpcp.php
@@ -810,7 +810,7 @@ class AWPCP {
 					'url' => 'http://www.awpcp.com/premium-modules/regions-control-module?ref=panel',
 					'installed' => defined( 'AWPCP_REGION_CONTROL_MODULE' ),
 					'version' => 'AWPCP_REGION_CONTROL_MODULE_DB_VERSION',
-					'required' => '3.5.2',
+					'required' => '3.5.6',
 				),
 				'restricted-categories' => array(
 					'name' => __( 'Restricted Categories', 'AWPCP' ),
