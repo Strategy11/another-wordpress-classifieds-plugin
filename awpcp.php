10bd532b372160529469a26b51bf9950430e37d4
Willington Vega <wvega@wvega.com>
2015-10-27 16:20:00 -0500

    Add Listings Media Uploader Component class.

--- awpcp.php
+++ awpcp.php
@@ -231,6 +231,7 @@ require_once( AWPCP_DIR . '/includes/media/class-image-file-processor.php' );
 require_once( AWPCP_DIR . '/includes/media/class-image-file-validator.php' );
 require( AWPCP_DIR . '/includes/media/class-image-media-creator.php' );
 require_once( AWPCP_DIR . '/includes/media/class-image-resizer.php' );
+require( AWPCP_DIR . '/includes/media/class-listings-media-uploader-component.php' );
 require_once( AWPCP_DIR . '/includes/media/class-listing-upload-limits.php' );
 require( AWPCP_DIR . '/includes/media/class-listing-media-creator.php' );
 require_once( AWPCP_DIR . "/includes/media/class-media-manager-component.php" );
--- includes/media/class-media-uploader-component.php
+++ includes/media/class-media-uploader-component.php
@@ -22,18 +22,6 @@ class AWPCP_MediaUploaderComponent {
 
         $this->javascript->set( 'media-uploader-data', $configuration );
 
-        $this->javascript->localize( 'media-uploader-strings', array(
-            'upload-restrictions-images-others-videos' => __( 'You can upload <images-left> images of up to <images-max-file-size> each, <videos-left> videos of up to <videos-max-file-size> each and <others-left> other files of up to <others-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-images-others' => __( 'You can upload <images-left> images of up to <images-max-file-size> each and <others-left> other files (no videos) of up to <others-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-images-videos' => __( 'You can upload <images-left> images of up to <images-max-file-size> each and <videos-left> videos of up to <videos-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-others-videos' => __( 'You can upload <videos-left> videos of up to <videos-max-file-size> each and <others-left> other files (no images) of up to <others-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-images' => __( 'You can upload <images-left> images of up to <images-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-others' => __( 'You can upload <others-left> files (no videos or images) of up to <others-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'upload-restrictions-videos' => __( 'You can upload <videos-left> videos of up to <videos-max-file-size> each.', 'another-wordpress-classifieds-plugin' ),
-            'cannot-add-more-files' => $this->validation_errors->get_cannot_add_more_files_of_type_error_message(),
-            'file-is-too-large' => $this->validation_errors->get_file_is_too_large_error_message(),
-        ) );
-
         return $this->render_component();
     }
 
