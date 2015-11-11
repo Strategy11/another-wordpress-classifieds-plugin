e4ba2c33669005feada971b396fff7f49309dd28
Willington Vega <wvega@wvega.com>
2015-11-11 12:24:51 -0500

    Add is_single_listing_page() method to AWPCP Query.

--- frontend/class-query.php
+++ frontend/class-query.php
@@ -12,6 +12,10 @@ function awpcp_query() {
  */
 class AWPCP_Query {
 
+    public function is_single_listing_page() {
+        return $this->is_page_that_has_shortcode( 'AWPCPSHOWAD' );
+    }
+
     public function is_browse_listings_page() {
         return $this->is_page_that_has_shortcode( 'AWPCPBROWSEADS' );
     }
