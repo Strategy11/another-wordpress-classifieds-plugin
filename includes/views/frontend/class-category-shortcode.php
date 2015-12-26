ef02ec0d05e3d3b674c016177333f52d5cdb367d
Willington Vega <wvega@wvega.com>
2015-12-26 17:45:46 -0500

    Allow AWPCPSHOWCAT to accept multiple categories IDs (#1437).

--- includes/views/frontend/class-category-shortcode.php
+++ includes/views/frontend/class-category-shortcode.php
@@ -41,29 +41,21 @@ class AWPCP_CategoryShortcode {
         $attrs['children'] = awpcp_parse_bool( $attrs['children'] );
         $attrs['show_categories_list'] = awpcp_parse_bool( $attrs['show_categories_list'] );
 
+        if ( strpos( $attrs['id'], ',' ) ){
+            $attrs['id'] = explode( ',', $attrs['id'] );
+        } else {
+            $attrs['id'] = array( $attrs['id'] );
+        }
+
         return $attrs;
     }
 
     private function render_shortcode_content( $attrs ) {
-        extract( $attrs );
-
-        $category = $id > 0 ? AWPCP_Category::find_by_id( $id ) : null;
-        $children = awpcp_parse_bool( $children );
-
-        if ( is_null( $category ) ) {
-            return __('Category ID must be valid for Ads to display.', 'category shortcode', 'another-wordpress-classifieds-plugin');
-        }
-
         if ( $attrs['show_categories_list'] ) {
-            $categories_list = awpcp_categories_list_renderer()->render( array(
-                'parent_category_id' => $category->id,
-                'show_listings_count' => true,
-            ) );
-
             $options = array(
                 'before_pagination' => array(
                     10 => array(
-                        'categories-list' => $categories_list,
+                        'categories-list' => $this->render_categories_list( $attrs['id'] ),
                     ),
                 ),
             );
@@ -73,16 +65,29 @@ class AWPCP_CategoryShortcode {
 
         $query = array(
             'context' => 'public-listings',
-            'category_id' => $category->id,
-            'include_listings_in_children_categories' => $children,
-            'limit' => absint( $this->request->param( 'results', $items_per_page ) ),
+            'category_id' => $attrs['id'],
+            'include_listings_in_children_categories' => $attrs['children'],
+            'limit' => absint( $this->request->param( 'results', $attrs['items_per_page'] ) ),
             'offset' => absint( $this->request->param( 'offset', 0 ) ),
             'orderby' => get_awpcp_option( 'groupbrowseadsby' ),
         );
 
-        // required so awpcp_display_ads shows the name of the current category
-        $_REQUEST['category_id'] = $category->id;
+        // // required so awpcp_display_ads shows the name of the current category
+        // // TODO: fix title when there are multiple categories ids.
+        // $_REQUEST['category_id'] = $category->id;
 
         return awpcp_display_listings_in_page( $query, 'category-shortcode', $options );
+    }
+
+    private function render_categories_list( $categories_ids ) {
+        $categories_list_params = array( 'show_listings_count' => true, );
+
+        if ( count( $categories_ids ) == 1 ) {
+            $categories_list_params['parent_category_id'] = $categories_ids[0];
+        } else {
+            $categories_list_params['category_id'] = $categories_ids;
+        }
+
+        return awpcp_categories_list_renderer()->render( $categories_list_params );
     }
 }
