308874bf9a4c6108f1dad03678f7c2a54d8e70e5
Willington Vega <wvega@wvega.com>
2016-02-06 23:22:02 -0500

    Add class Listing Attachment Creator (#1112).
    It replaces Listing Media Creator.

---
+++ includes/media/class-listing-attachment-creator.php
@@ -0,0 +1,30 @@
+<?php
+
+function awpcp_listing_attachment_creator() {
+    return new AWPCP_Listing_Attachment_Creator( awpcp_wordpress() );
+}
+
+class AWPCP_Listing_Attachment_Creator {
+
+    private $wordpress;
+
+    public function __construct( $wordpress ) {
+        $this->wordpress = $wordpress;
+    }
+
+    public function create_attachment( $listing, $file_logic, $allowed_status ) {
+        $attachment_id = $this->wordpress->handle_media_sideload(
+            array(
+                'name' => $file_logic->get_real_name(),
+                'tmp_name' => $file_logic->get_path(),
+            ),
+            $listing->ID,
+            ''
+        );
+
+        $this->wordpress->update_post_meta( $attachment_id, '_enabled', true );
+        $this->wordpress->update_post_meta( $attachment_id, '_allowed_status', $allowed_status );
+
+        return get_post( $attachment_id );
+    }
+}
