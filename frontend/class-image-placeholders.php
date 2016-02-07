5194823be02ecd4548bae36cec74fbf18478d07f
Willington Vega <wvega@wvega.com>
2016-02-06 22:58:16 -0500

    Render images using wp_get_attachment_image (#1112).

--- frontend/class-image-placeholders.php
+++ frontend/class-image-placeholders.php
@@ -63,44 +63,27 @@ class AWPCP_Image_Placeholders {
                     $link = '';
                 }
 
-                $image_dimensions = $this->media_api->get_metadata( $primary_image, 'image-dimensions', array() );
-                $image_dimensions = awpcp_array_data( 'primary', array(), $image_dimensions );
-
                 $link_attributes = array(
                     'class' => 'awpcp-listing-primary-image-thickbox-link thickbox thumbnail',
                     'href' => esc_url( $large_image ),
                     'rel' => esc_attr( $gallery_name ),
                 );
 
-                // single ad
-                $image_attributes = array(
-                    'attributes' => array(
-                        'class' => 'thumbshow',
-                        'src' => esc_attr( $thumbnail ),
-                        'width' => awpcp_array_data( 'width', null, $image_dimensions ),
-                        'height' => awpcp_array_data( 'height', null, $image_dimensions ),
-                    )
-                );
-
                 $content = '<div class="awpcp-ad-primary-image">';
                 $content.= '<a ' . awpcp_html_attributes( $link_attributes ) . '>';
-                $content.= awpcp_html_image( $image_attributes );
+                $content.= wp_get_attachment_image( $primary_image->ID, 'awpcp-featured', false, array( 'class' => 'thumbshow' ) );
                 $content.= '</a>' . $link;
                 $content.= '</div>';
 
                 $placeholders['featureimg'] = $content;
 
                 // listings
-                $image_attributes = array(
-                    'attributes' => array(
-                        'alt' => awpcp_esc_attr( awpcp_listing_renderer()->get_listing_title( $ad ) ),
-                        'src' => esc_attr( $thumbnail ),
-                        'width' => $thumbnail_width,
-                    )
-                );
+                $image_attributes = array( 'alt' => awpcp_esc_attr( awpcp_listing_renderer()->get_listing_title( $ad ) ) );
 
                 $content = '<a class="awpcp-listing-primary-image-listing-link" href="%s">%s</a>';
-                $content = sprintf( $content, $url, awpcp_html_image( $image_attributes ) );
+                // TODO: Can we define a custom image size everytime the user changes the displayadthumbwidth setting, and regenerate
+                //       thumbnails for that.
+                $content = sprintf( $content, $url, wp_get_attachment_image( $primary_image->ID, array( $thumbnail_width, 0 ), false, $image_attributes ) );
 
                 $placeholders['awpcp_image_name_srccode'] = $content;
             }
@@ -121,10 +104,6 @@ class AWPCP_Image_Placeholders {
                     }
 
                     $large_image = $this->attachment_properties->get_image_url( $image, 'large' );
-                    $thumbnail = $this->attachment_properties->get_image_url( $image, 'thumbnail' );
-
-                    $image_dimensions = awpcp_media_api()->get_metadata( $image, 'image-dimensions', array() );
-                    $image_dimensions = awpcp_array_data( 'thumbnail', array(), $image_dimensions );
 
                     if ($columns > 0) {
                         $li_attributes['class'] = join(' ', awpcp_get_grid_item_css_class(array(), $shown, $columns, $rows));
@@ -138,18 +117,9 @@ class AWPCP_Image_Placeholders {
                         'rel' => esc_attr( $gallery_name )
                     );
 
-                    $image_attributes = array(
-                        'attributes' => array(
-                            'class' => 'thumbshow',
-                            'src' => esc_attr( $thumbnail ),
-                            'width' => awpcp_array_data( 'width', null, $image_dimensions ),
-                            'height' => awpcp_array_data( 'height', null, $image_dimensions ),
-                        )
-                    );
-
                     $content = '<li ' . awpcp_html_attributes( $li_attributes ) . '>';
                     $content.= '<a ' . awpcp_html_attributes( $link_attributes ) . '>';
-                    $content.= awpcp_html_image( $image_attributes );
+                    $content.= wp_get_attachment_image( $image->ID, 'awpcp-thumbnail', false, array( 'class' => 'thumbshow' ) );
                     $content.= '</a>';
                     $content.= '</li>';
 
