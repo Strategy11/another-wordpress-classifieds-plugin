19fd84fcd3700d0b3b68614cc474683ec99cddad
Willington Vega <wvega@wvega.com>
2016-02-11 11:16:11 -0500

    Add more methods to get information about attachments in Attachment Properties (#1112).

--- includes/media/class-attachment-properties.php
+++ includes/media/class-attachment-properties.php
@@ -12,8 +12,33 @@ class AWPCP_Attachment_Properties {
         $this->wordpress = $wordpress;
     }
 
+    public function is_enabled( $attachment ) {
+        return $this->wordpress->get_post_meta( $attachment->ID, '_enabled', true );
+    }
+
+    public function is_featured( $attachment ) {
+        return $this->wordpress->get_post_meta( $attachment->ID, '_featured', true );
+    }
+
+    public function get_allowed_status( $attachment ) {
+        return $this->wordpress->get_post_meta( $attachment->ID, '_allowed_status', true );
+    }
+
+    public function is_awaiting_approval( $attachment ) {
+        return $this->get_allowed_status( $attachment ) == AWPCP_Attachment_Status::STATUS_AWAITING_APPROVAL;
+    }
+
+    public function is_image( $attachment ) {
+        return in_array( $attachment->post_mime_type, awpcp_get_image_mime_types() );
+    }
+
     public function get_image_url( $attachment, $size ) {
         return $this->wordpress->get_attachment_image_url( $attachment->ID, "awpcp-$size" );
+    }
+
+    public function get_icon_url( $attachment ) {
+        $src = $this->wordpress->get_attachment_image_src( $attachment->ID, "awpcp-thumbnail", true );
+        return is_array( $src ) ? $src[0] : null;
     }
 
     public function get_image( $attachment, $size, $ah, $attributes ) {
