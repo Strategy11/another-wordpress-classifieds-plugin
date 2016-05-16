9e20e2a915952178f6bd1b380da8050a9d352f46
Willington Vega <wvega@wvega.com>
2016-05-15 19:19:02 -0500

    Fix get_id_of_associated_listing() in upgrade routine to migrate media.
    The previous version caused all images to be associated with first lising created.

--- includes/upgrade/class-store-media-as-attachments-upgrade-task-handler.php
+++ includes/upgrade/class-store-media-as-attachments-upgrade-task-handler.php
@@ -104,10 +104,8 @@ class AWPCP_Store_Media_As_Attachments_Upgrade_Task_Handler implements AWPCP_Upg
     private function get_id_of_associated_listing( $item ) {
         $listings = get_posts( array(
             'post_type' => 'awpcp_listing',
-            'meta_query' => array(
-                'key' => '_awpcp_old_id',
-                'value' => $item->ad_id,
-            ),
+            'meta_key' => '_awpcp_old_id',
+            'meta_value' => $item->ad_id,
         ) );
 
         if ( ! is_array( $listings ) || empty( $listings ) ) {
