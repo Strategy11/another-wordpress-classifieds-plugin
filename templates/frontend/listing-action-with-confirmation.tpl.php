e273a8924277ded8f24cd6165065577e5e9fce22
Willington Vega <wvega@wvega.com>
2016-06-20 19:26:51 -0500

    Prevent PHP notice in template.

--- templates/frontend/listing-action-with-confirmation.tpl.php
+++ templates/frontend/listing-action-with-confirmation.tpl.php
@@ -1,8 +1,6 @@
 <?php $css_class = esc_attr( "awpcp-listing-action-" . $this->get_slug() ); ?>
 <form class="awpcp-listing-action-form awpcp-listing-action-form-with-confirmation <?php echo $css_class; ?>-form <?php echo $css_class; ?>-form-with-confirmation" action="<?php echo esc_attr( $this->get_endpoint( $listing, $config ) ); ?>" method="post">
-    <?php foreach ( $this->filter_params( $config['hidden-params'] ) as $name => $value ): ?>
-    <input type="hidden" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $value ); ?>" />
-    <?php endforeach; ?>
+    <?php echo awpcp_html_hidden_fields( $this->filter_params( $config['hidden-params'] ) ); ?>
 
     <!-- <span class="<?php echo $css_class; ?>-description"><?php echo esc_html( $this->get_description() ); ?></span>-->
     <span class="awpcp-listing-action-form-confirmation is-hidden"><?php echo esc_html( $this->get_confirmation_message() ); ?></span>
