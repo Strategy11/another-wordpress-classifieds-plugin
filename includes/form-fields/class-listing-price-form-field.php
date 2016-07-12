cf7d640da897e9d34aee62e8f014cb3d471fe9f8
Willington Vega <wvega@wvega.com>
2016-07-12 12:29:51 -0500

    Show currency symbol next to price field (#1680).

--- includes/form-fields/class-listing-price-form-field.php
+++ includes/form-fields/class-listing-price-form-field.php
@@ -57,8 +57,15 @@ class AWPCP_ListingPriceFormField extends AWPCP_FormField {
                 'name' => $this->get_slug(),
                 'readonly' => false,
             ),
+
+            'currency_symbol' => awpcp_get_currency_symbol(),
+            'show_currency_symbol_on_right' => $this->should_show_currency_symbol_on_right(),
         );
 
-        return awpcp_render_template( 'frontend/form-fields/listing-contact-phone-form-field.tpl.php', $params );
+        return awpcp_render_template( 'frontend/form-fields/listing-price-form-field.tpl.php', $params );
+    }
+
+    private function should_show_currency_symbol_on_right() {
+        return $this->settings->get_option( 'show-currency-symbol' ) == 'show-currency-symbol-on-right';
     }
 }
