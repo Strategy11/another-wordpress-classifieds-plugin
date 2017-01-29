76ab61e57546f41aeebc237cf29b7de61a3b0ad5
Willington Vega <wvega@wvega.com>
2017-01-29 18:50:03 -0500

    Merge functions to verify PayPal received data.
    The version in functions/payments.php weere using old URLs, causing
    false alarm errors in the debug page.

--- includes/payment-gateway-paypal-standard.php
+++ includes/payment-gateway-paypal-standard.php
@@ -24,130 +24,6 @@ class AWPCP_PayPalStandardPaymentGateway extends AWPCP_PaymentGateway {
         return self::INTEGRATION_BUTTON;
     }
 
-    /**
-     * Verify data received from PayPal IPN notifications using cURL and
-     * returns PayPal's response.
-     *
-     * Request errors, if any, are returned by reference.
-     *
-     *
-     * @return VERIFIED, INVALID or ERROR
-     * @since 2.1.4
-     */
-    private function verify_recevied_data_with_curl($postfields='', $cainfo=true, &$errors=array()) {
-        if (get_awpcp_option('paylivetestmode') == 1) {
-            $paypal_url = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
-        } else {
-            $paypal_url = "https://ipnpb.paypal.com/cgi-bin/webscr";
-        }
-
-        $ch = curl_init($paypal_url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-
-        if ($cainfo)
-            curl_setopt($ch, CURLOPT_CAINFO, AWPCP_DIR . '/cacert.pem');
-
-        $result = curl_exec($ch);
-        if (in_array($result, array('VERIFIED', 'INVALID'))) {
-            $response = $result;
-        } else {
-            $response = 'ERROR';
-        }
-
-        if (curl_errno($ch)) {
-            $errors[] = sprintf('%d: %s', curl_errno($ch), curl_error($ch));
-        }
-
-        curl_close($ch);
-
-        return $response;
-    }
-
-    /**
-     * Verify data received from PayPal IPN notifications using fsockopen and
-     * returns PayPal's response.
-     *
-     * Request errors, if any, are returned by reference.
-     *
-     * @since 2.1.1
-     */
-    private function verify_received_data_with_fsockopen($content, &$errors=array()) {
-        if (get_awpcp_option('paylivetestmode') == 1) {
-            $host = "test-ipnpb.sandbox.paypal.com";
-        } else {
-            $host = "ipnpb.paypal.com";
-        }
-
-        $response = 'ERROR';
-
-        // post back to PayPal system to validate
-        $header = "POST /cgi-bin/webscr HTTP/1.1\r\n";
-        $header.= "Host: $host\r\n";
-        $header.= "Connection: close\r\n";
-        $header.= "Content-Type: application/x-www-form-urlencoded\r\n";
-        $header.= "Content-Length: " . strlen($content) . "\r\n\r\n";
-        $fp = fsockopen("ssl://$host", 443, $errno, $errstr, 30);
-
-        if ($fp) {
-            fputs ($fp, $header . $content);
-
-            while(!feof($fp)) {
-                $line = fgets($fp, 1024);
-                if (strcasecmp($line, "VERIFIED") == 0 || strcasecmp($line, "INVALID") == 0) {
-                    $response = $line;
-                    break;
-                }
-            }
-
-            fclose($fp);
-        } else {
-            $errors[] = sprintf('%d: %s', $errno, $errstr);
-        }
-
-        return $response;
-    }
-
-    /**
-     * Verify data received from PayPal IPN notifications and returns PayPal's
-     * response.
-     *
-     * Request errors, if any, are returned by reference.
-     *
-     * @return VERIFIED, INVALID or ERROR
-     * @since 2.0.7
-     */
-    private function verify_received_data($data=array(), &$errors=array()) {
-        $content = 'cmd=_notify-validate';
-        foreach ($data as $key => $value) {
-            $value = urlencode(stripslashes($value));
-            $content .= "&$key=$value";
-        }
-
-        $response = 'ERROR';
-        if (in_array('curl', get_loaded_extensions())) {
-            // try using custom CA information -- included with the plugin
-            $response = $this->verify_recevied_data_with_curl($content, true, $errors);
-
-            // try using default CA information -- installed in the server
-            if (strcmp($response, 'ERROR') === 0)
-                $response = $this->verify_recevied_data_with_curl($content, false, $errors);
-        }
-
-        if (strcmp($response, 'ERROR') === 0)
-            $response = $this->verify_received_data_with_fsockopen($content, $errors);
-
-        return $response;
-    }
-
     private function verify_transaction($transaction) {
         $errors = array();
 
@@ -156,7 +32,7 @@ class AWPCP_PayPalStandardPaymentGateway extends AWPCP_PaymentGateway {
         // already verified during the POST request the result
         // should be stored in the transaction's verified attribute
         if (!empty($_POST)) {
-            $response = $this->verify_received_data($_POST, $errors);
+            $response = awpcp_paypal_verify_received_data( $_POST, $errors );
             $verified = strcasecmp($response, 'VERIFIED') === 0;
         } else {
             $response = null;
