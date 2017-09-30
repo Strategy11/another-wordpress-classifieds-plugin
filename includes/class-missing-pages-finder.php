ef6ac43b86933a1a7cd8f1495ed075f25a51dfaa
Willington Vega <wvega@wvega.com>
2017-09-30 10:18:26 -0500

    Change behavior of Restore Pages button.
    The plugin now attempts to find the pages, update the
    page id references, and then, if some pages are still
    missing, then it tries to create them again.

--- includes/class-missing-pages-finder.php
+++ includes/class-missing-pages-finder.php
@@ -12,7 +12,20 @@ class AWPCP_Missing_Pages_Finder {
         $this->db = $db;
     }
 
-    public function find_missing_pages() {
+    public function find_page_with_shortcode( $shortcode ) {
+        $query = 'SELECT ID, post_title, post_status ';
+        $query.= 'FROM ' . $this->db->posts . ' ';
+        $query.= "WHERE post_content LIKE '%%%s%%' ";
+        $query.= "AND post_status NOT IN ('inherit') ";
+
+        $sql = $this->db->prepare( $query, $shortcode );
+
+        $pages = $this->db->get_results( $sql );
+
+        return count( $pages ) === 1 ? $pages[0] : null;
+    }
+
+    public function find_broken_page_id_references() {
         $plugin_pages = awpcp_get_plugin_pages_info();
 
         $registered_pages = array_keys( awpcp_pages() );
