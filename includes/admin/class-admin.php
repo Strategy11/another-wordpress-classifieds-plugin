eec37049751e94fcc5b0669064bec065bb9b855f
Willington Vega <wvega@wvega.com>
2018-04-03 21:24:48 -0500

    Register tasks for our post type only.

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -32,9 +32,13 @@ class AWPCP_Admin {
      * @since 4.0.0
      */
     public function admin_init() {
-        add_action( 'admin_head-edit.php', array( $this->table_actions, 'admin_head' ), 10, 2 );
-        add_action( 'post_row_actions', array( $this->table_actions, 'row_actions' ), 10, 2 );
-        add_filter( 'handle_bulk_actions-edit-' . $this->table_actions->get_post_type(), array( $this->table_actions, 'handle_action' ), 10, 3 );
+        global $typenow;
+
+        if ( $this->table_actions->get_post_type() === $typenow ) {
+            add_action( 'admin_head-edit.php', array( $this->table_actions, 'admin_head' ), 10, 2 );
+            add_action( 'post_row_actions', array( $this->table_actions, 'row_actions' ), 10, 2 );
+            add_filter( 'handle_bulk_actions-edit-' . $this->table_actions->get_post_type(), array( $this->table_actions, 'handle_action' ), 10, 3 );
+        }
 
         add_filter( 'awpcp_list_table_actions_listings', array( $this, 'register_listings_table_actions' ) );
     }
