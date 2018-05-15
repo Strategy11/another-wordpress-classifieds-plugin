dc9fe84dd66ddde13bd3bf10a0728b917e5dd004
Willington Vega <wvega@wvega.com>
2018-05-15 11:38:25 -0500

    Fix PHPCS and PHPMD issues.

--- includes/media/class-listing-file-handler.php
+++ includes/media/class-listing-file-handler.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * @package AWPCP\Media
+ */
 
+/**
+ * Constructor function.
+ */
 function awpcp_image_file_handler() {
     return new AWPCP_ListingFileHandler(
         awpcp_image_file_validator(),
@@ -8,25 +14,50 @@ function awpcp_image_file_handler() {
     );
 }
 
+/**
+ * Validates, process and associates an uploaded file with a listing.
+ */
 class AWPCP_ListingFileHandler {
 
+    /**
+     * @var object
+     */
     private $validator;
+
+    /**
+     * @var object
+     */
     private $processor;
+
+    /**
+     * @var object
+     */
     private $creator;
 
+    /**
+     * @param object $validator     An instance of Listing File Validator.
+     * @param object $processor     An instance of Listing File Processor.
+     * @param object $creator       An instance of Listing Attachment Creator.
+     */
     public function __construct( $validator, $processor, $creator ) {
         $this->validator = $validator;
         $this->processor = $processor;
-        $this->creator = $creator;
+        $this->creator   = $creator;
     }
 
     /**
+     * @param object $listing   An instance of WP_Post.
+     * @param object $file      An instance of Uploaded File Logic.
      * @since 4.0.0
      */
     public function validate_file( $listing, $file ) {
         $this->validator->validate_file( $listing, $file );
     }
 
+    /**
+     * @param object $listing   An instance of WP_Post.
+     * @param object $file      An instance of Uploaded File Logic.
+     */
     public function handle_file( $listing, $file ) {
         $this->validator->validate_file( $listing, $file );
         $this->processor->process_file( $listing, $file );
--- includes/media/class-media-manager.php
+++ includes/media/class-media-manager.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * @package AWPCP\Media
+ */
 
+/**
+ * Constructor function.
+ */
 function awpcp_new_media_manager() {
     static $instance = null;
 
@@ -14,31 +20,59 @@ function awpcp_new_media_manager() {
     return $instance;
 }
 
+/**
+ * Logic for adding media files to listings.
+ */
 class AWPCP_Media_Manager {
 
+    /**
+     * @var object
+     */
     private $file_handlers;
+
+    /**
+     * @var object
+     */
     private $uploaded_file_logic_factory;
+
+    /**
+     * @var object
+     */
     private $settings;
 
+    /**
+     * @param object $file_handlers                 An instance of File Handlers.
+     * @param object $uploaded_file_logic_factory   An instance of Uploaded File Logic Factory.
+     * @param object $settings                      An instance of Settings.
+     */
     public function __construct( $file_handlers, $uploaded_file_logic_factory, $settings ) {
-        $this->file_handlers = $file_handlers;
+        $this->file_handlers               = $file_handlers;
         $this->uploaded_file_logic_factory = $uploaded_file_logic_factory;
-        $this->settings = $settings;
+        $this->settings                    = $settings;
     }
 
     /**
+     * @param object $listing           An instance of WP_Post.
+     * @param object $uploaded_file     An object with information about the file
+     *                                  that is being added.
      * @since 4.0.0
      */
     public function validate_file( $listing, $uploaded_file ) {
-        $file_logic = $this->uploaded_file_logic_factory->create_file_logic( $uploaded_file );
+        $file_logic   = $this->uploaded_file_logic_factory->create_file_logic( $uploaded_file );
         $file_handler = $this->file_handlers->get_handler_for_file( $file_logic );
 
         return $file_handler->validate_file( $listing, $file_logic );
     }
 
+    /**
+     * @param object $listing           An instance of WP_Post.
+     * @param object $uploaded_file     An object with information about the file
+     *                                  that is being added.
+     */
     public function add_file( $listing, $uploaded_file ) {
-        $file_logic = $this->uploaded_file_logic_factory->create_file_logic( $uploaded_file );
+        $file_logic   = $this->uploaded_file_logic_factory->create_file_logic( $uploaded_file );
         $file_handler = $this->file_handlers->get_handler_for_file( $file_logic );
+
         return $file_handler->handle_file( $listing, $file_logic );
     }
 }
