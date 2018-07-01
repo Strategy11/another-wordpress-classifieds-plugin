5d788ca9d84a0f4d56c085b6fde3474592bc7233
Willington Vega <wvega@wvega.com>
2018-07-01 14:31:08 -0500

    Fix PHPCS issues.

--- includes/media/class-listing-attachment-creator.php
+++ includes/media/class-listing-attachment-creator.php
@@ -1,21 +1,39 @@
 <?php
+/**
+ * @package AWPCP\Media
+ */
 
+/**
+ * Constructor for Listing Attachment Creator.
+ */
 function awpcp_listing_attachment_creator() {
     return new AWPCP_Listing_Attachment_Creator( awpcp_wordpress() );
 }
 
+/**
+ * Service used to create attachments associated with listings.
+ */
 class AWPCP_Listing_Attachment_Creator {
 
+    /**
+     * @var WordPress
+     */
     private $wordpress;
 
+    /**
+     * Constructor.
+     */
     public function __construct( $wordpress ) {
         $this->wordpress = $wordpress;
     }
 
+    /**
+     * Creates an attachment associated with the given listing.
+     */
     public function create_attachment( $listing, $file_logic, $allowed_status = AWPCP_Attachment_Status::STATUS_APPROVED ) {
         $attachment_id = $this->wordpress->handle_media_sideload(
             array(
-                'name' => $file_logic->get_real_name(),
+                'name'     => $file_logic->get_real_name(),
                 'tmp_name' => $file_logic->get_path(),
             ),
             $listing->ID,
