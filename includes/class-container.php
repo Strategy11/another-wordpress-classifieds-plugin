30d31866c4bbf9565cc8a4ed4f6ca7641381be4f
Willington Vega <wvega@wvega.com>
2018-08-03 21:45:05 -0500

    Fix PHPCS and PHPMD issues.

--- includes/class-container.php
+++ includes/class-container.php
@@ -17,13 +17,20 @@ class AWPCP_Container implements ArrayAccess {
      */
     private $values = array();
 
+    /**
+     * @var array
+     */
     private $definitions = array();
+
+    /**
+     * @var array
+     */
     private $shared = array();
 
     /**
      * Constructor
      *
-     * @param array $values
+     * @param array $values     Initial set of values to store in the container.
      */
     public function __construct( array $values = array() ) {
         $this->values = $values;
@@ -32,7 +39,7 @@ class AWPCP_Container implements ArrayAccess {
     /**
      * Checks if there's a value in the container for the given key.
      *
-     * @param mixed $key
+     * @param mixed $key    The identifier of a value in the container.
      *
      * @return bool
      */
@@ -43,7 +50,9 @@ class AWPCP_Container implements ArrayAccess {
     /**
      * Returns a value stored in the container for the given key.
      *
-     * @param mixed $key
+     * @param mixed $key    The identifier of a value in the container.
+     * @throws Exception    If the container doesn't have a value assocaited with
+     *                      the given $key.
      */
     public function offsetGet( $key ) {
         if ( ! array_key_exists( $key, $this->values ) ) {
@@ -60,8 +69,8 @@ class AWPCP_Container implements ArrayAccess {
     /**
      * Sets a value inside the container.
      *
-     * @param mixed $key
-     * @param mixed $value
+     * @param mixed $key    The identifier for the new value.
+     * @param mixed $value  The value to store in the container.
      */
     public function offsetSet( $key, $value ) {
         $this->values[ $key ] = $value;
@@ -70,7 +79,7 @@ class AWPCP_Container implements ArrayAccess {
     /**
      * Unsets the value in the container for the given key.
      *
-     * @param mixed $key
+     * @param mixed $key    The identifier of a value in the container.
      */
     public function offsetUnset( $key ) {
         unset( $this->values[ $key ] );
@@ -79,21 +88,23 @@ class AWPCP_Container implements ArrayAccess {
     /**
      * Configure the container using the given configuration objects.
      *
-     * @param array $configurations
+     * @param array $configurations     An array of instances of Container Configurations Interface.
      */
     public function configure( array $configurations ) {
-        foreach( $configurations as $configuration ) {
+        foreach ( $configurations as $configuration ) {
             $configuration->modify( $this );
         }
     }
+
+    // phpcs:disable Squiz.Commenting.FunctionComment.IncorrectTypeHint
 
     /**
      * Returns a closure that creates a service object using the given constructor
      * function.
      *
-     * @param Closure A constructor function for the object.
+     * @param Closur $closure   A constructor function for the object.
      *
-     * @return Closue A constructor function for the service object.
+     * @return Closue   A constructor function for the service object.
      */
     public function service( Closure $closure ) {
         return function ( $container ) use ( $closure ) {
@@ -107,15 +118,23 @@ class AWPCP_Container implements ArrayAccess {
         };
     }
 
+    // phpcs:enable Squiz.Commenting.FunctionComment.IncorrectTypeHint
+
+    /**
+     * @deprecated 4.0.0    Register the constructor function as a service.
+     */
     public function share( $name, $files, $constructor ) {
         $this->definitions[ $name ] = array(
-            'name' => $name,
-            'files' => $files,
-            'constructor' => $constructor,
+            'name'          => $name,
+            'files'         => $files,
+            'constructor'   => $constructor,
             'instance-type' => 'shared',
         );
     }
 
+    /**
+     * @deprecated 4.0.0    Use the container as an array instead.
+     */
     public function get( $name ) {
         if ( ! isset( $this->definitions[ $name ] ) ) {
             return null;
@@ -123,24 +142,33 @@ class AWPCP_Container implements ArrayAccess {
 
         $definition = $this->definitions[ $name ];
 
-        if ( $definition['instance-type'] == 'shared' ) {
+        if ( 'shared' === $definition['instance-type'] ) {
             return $this->get_shared_instance( $definition );
-        } else {
-            return $this->get_instance( $definition );
         }
+
+        return $this->get_instance( $definition );
     }
 
+    /**
+     * @deprecated 4.0.0    Register the constructor function as a service and
+     *                      use the container as an array instead.
+     */
     private function get_shared_instance( $definition ) {
         if ( isset( $this->shared[ $definition['name'] ] ) ) {
             return $this->shared[ $definition['name'] ];
         }
 
-        return $this->shared[ $definition['name'] ] = $this->get_instance( $definition );
+        $this->shared[ $definition['name'] ] = $this->get_instance( $definition );
+
+        return $this->shared[ $definition['name'] ];
     }
 
+    /**
+     * @deprecated 4.0.0    Use the container as an array instead.
+     */
     private function get_instance( $definition ) {
         foreach ( (array) $definition['files'] as $file ) {
-            require_once( $file );
+            require_once $file;
         }
 
         if ( ! is_callable( $definition['constructor'] ) ) {
