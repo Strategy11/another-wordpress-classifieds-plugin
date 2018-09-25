57069a25b479d0e329552225b0fbc2cf7c49e0de
Willington Vega <wvega@wvega.com>
2018-09-25 18:08:01 -0500

    Show Enable Listing action for listings pending approval.

--- includes/admin/listings/class-enable-listing-table-action.php
+++ includes/admin/listings/class-enable-listing-table-action.php
@@ -33,7 +33,7 @@ class AWPCP_EnableListingTableAction implements AWPCP_ListTableActionInterface {
      * @since 4.0.0
      */
     public function should_show_action_for( $post ) {
-        return $this->listing_renderer->is_disabled( $post );
+        return $this->listing_renderer->is_disabled( $post ) || $this->listing_renderer->is_pending_approval( $post );
     }
 
     /**
