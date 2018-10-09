dbe00546acb3975c25a9cc83a899d16bd4284fe4
Willington Vega <wvega@wvega.com>
2018-10-09 18:50:09 -0500

    Radio settings now accept a callback to generate the options.
    A new version of e9f3ffca.

--- includes/settings/renderers/class-radio-settings-renderer.php
+++ includes/settings/renderers/class-radio-settings-renderer.php
@@ -27,17 +27,33 @@ class AWPCP_RadioSettingsRenderer {
      */
     public function render_setting( $setting ) {
         $current = esc_html( stripslashes( $this->settings->get_option( $setting['id'] ) ) );
+        $options = array();
+
+        if ( is_array( $setting['options'] ) ) {
+            $options = $setting['options'];
+        }
+
+        if ( is_callable( $setting['options'] ) ) {
+            $options = call_user_func( $setting['options'] );
+        }
 
         $html = '';
 
-        foreach ( $setting['options'] as $value => $label ) {
-            $id    = "{$setting['id']}-$value";
+        foreach ( $options as $key => $label ) {
+            $value = $key;
+
+            if ( is_array( $label ) ) {
+                $value = $label['value'];
+                $label = $label['label'];
+            }
+
+            $id    = "{$setting['id']}-$key";
             $label = ' <label for="' . esc_attr( $id ) . '">' . $label . '</label>';
 
             $html .= '<input id="' . esc_attr( $id ) . '"type="radio" value="' . esc_attr( $value ) . '" ';
             $html .= 'name="awpcp-options[' . $setting['id'] . ']" ';
 
-            if ( 0 === strcmp( (string) $value, $current ) ) {
+            if ( 0 === strcmp( (string) $key, $current ) ) {
                 $html .= 'checked="checked" />' . $label;
             } else {
                 $html .= '>' . $label;
