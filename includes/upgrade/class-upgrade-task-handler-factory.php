6109a8cafd5a2649206f38febf7862d3a31f1b54
Willington Vega <wvega@wvega.com>
2018-10-20 16:55:27 -0500

    Add documentation for Upgrade Task Handler Factory.

--- includes/upgrade/class-upgrade-task-handler-factory.php
+++ includes/upgrade/class-upgrade-task-handler-factory.php
@@ -1,13 +1,29 @@
 <?php
+/**
+ * @package AWPCP\Upgrade
+ */
 
+/**
+ * Factory for Upgrade Task Handler class.
+ */
 class AWPCP_Upgrade_Task_Handler_Factory {
 
+    /**
+     * @var Container
+     */
     private $container;
 
+    /**
+     * Constructor.
+     */
     public function __construct( $container ) {
         $this->container = $container;
     }
 
+    /**
+     * Loads an upgrade task handler using an instance of the provided class
+     * name as the Task Runner.
+     */
     public function get_task_handler( $task_runner_class ) {
         $task_runner = null;
 
