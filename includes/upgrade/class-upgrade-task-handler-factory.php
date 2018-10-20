cb6528dfd84f17dc0bdb03c27a5f120a01d4bc5e
Willington Vega <wvega@wvega.com>
2018-10-20 16:54:27 -0500

    Register upgrade routines using the new container interface.

--- includes/upgrade/class-upgrade-task-handler-factory.php
+++ includes/upgrade/class-upgrade-task-handler-factory.php
@@ -9,8 +9,18 @@ class AWPCP_Upgrade_Task_Handler_Factory {
     }
 
     public function get_task_handler( $task_runner_class ) {
+        $task_runner = null;
+
+        if ( isset( $this->container[ $task_runner_class ] ) ) {
+            $task_runner = $this->container[ $task_runner_class ];
+        }
+
+        if ( is_null( $task_runner ) ) {
+            $task_runner = $this->container->get( $task_runner_class );
+        }
+
         return new AWPCP_Upgrade_Task_Handler(
-            $this->container->get( $task_runner_class ),
+            $task_runner,
             $this->container->get( 'AWPCP_Upgrade_Sessions' ),
             awpcp_upgrade_tasks_manager()
         );
