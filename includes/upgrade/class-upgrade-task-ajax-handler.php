9f0f47fcc3234ebfeca175e23914c7359b562b9c
Willington Vega <wvega@wvega.com>
2018-11-23 20:08:12 -0500

    Fix PHPCS issues in class-upgrade-task-ajax-handler.php.

--- includes/upgrade/class-upgrade-task-ajax-handler.php
+++ includes/upgrade/class-upgrade-task-ajax-handler.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @package AWPCP\Upgrade
+ */
+
+// @phpcs:disable Squiz.Commenting
 
 class AWPCP_Upgrade_Task_Ajax_Handler extends AWPCP_AjaxHandler {
 
@@ -9,19 +14,19 @@ class AWPCP_Upgrade_Task_Ajax_Handler extends AWPCP_AjaxHandler {
     public function __construct( $tasks_manager, $task_handler_factory, $request, $response ) {
         parent::__construct( $response );
 
-        $this->tasks_manager = $tasks_manager;
+        $this->tasks_manager        = $tasks_manager;
         $this->task_handler_factory = $task_handler_factory;
-        $this->request = $request;
+        $this->request              = $request;
     }
 
     public function ajax() {
         $task_slug = $this->request->param( 'action' );
-        $context = $this->request->param( 'context' );
+        $context   = $this->request->param( 'context' );
 
         $task = $this->tasks_manager->get_upgrade_task( $task_slug );
 
         if ( is_null( $task ) ) {
-            return $this->error_response( sprintf( "No task was found with identifier: %s.", $task_slug ) );
+            return $this->error_response( sprintf( 'No task was found with identifier: %s.', $task_slug ) );
         }
 
         $task_handler = $this->task_handler_factory->get_task_handler( $task['handler'] );
