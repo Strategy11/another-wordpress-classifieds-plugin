c82419b3292c23ab6d613c8f0386fd891c14a5dc
Willington Vega <wvega@wvega.com>
2018-12-12 18:08:16 -0500

    Add .h-awpcp-nomargin-nopadding CSS helper.

--- templates/components/form-steps.tpl.php
+++ templates/components/form-steps.tpl.php
@@ -1,5 +1,5 @@
 <div class="awpcp-form-steps">
-    <ul>
+    <ul class="m-awpcp-form-steps__list">
     <?php foreach ( $form_steps as $step ):
         ?><li class="awpcp-form-steps-step awpcp-form-steps-<?php echo esc_attr( $step['class'] ); ?>-step">
             <span class="awpcp-form-steps-step-inner" title="<?php echo esc_attr( $step['name'] ); ?>">
