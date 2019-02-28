60817eeafb996aa230f4b6e238845cc65fc56861
Juan <juan@freerange.com>
2019-02-28 14:40:42 +0100

    refactoring and changes

--- includes/compatibility/class-seo-framework-plugin-integration.php
+++ includes/compatibility/class-seo-framework-plugin-integration.php
@@ -64,7 +64,12 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	private function are_required_classes_loaded() {
-		return defined( 'THE_SEO_FRAMEWORK_VERSION' );
+		if ( ! defined( 'THE_SEO_FRAMEWORK_VERSION' ) ) {
+			// Yoast SEO doesn't seem to be loaded. Bail.
+			return false;
+		}
+
+		return class_exists( 'The_SEO_Framework\Post_Data' );
 	}
 
 	/**
@@ -75,11 +80,51 @@ class AWPCP_SEOFrameworkIntegration {
 		$this->title_builder   = $meta->title_builder;
 		$this->is_singular     = is_singular( $this->listing_post_type );
 		$this->metadata        = [];
+		$this->seo             = the_seo_framework();
 
 		if ( $this->current_listing ) {
 			$this->metadata = $meta->get_listing_metadata();
 		}
+
+		add_filter( 'awpcp-should-generate-rel-canonical', [ $this, 'configure_canonical_url' ] );
+		add_filter( 'awpcp-should-generate-title', [ $this, 'configure_title_generation' ] );
+		add_filter( 'awpcp-should-generate-basic-meta-tags', [ $this, 'configure_description_meta_tags' ] );
 		add_filter( 'awpcp-should-generate-opengraph-tags', [ $this, 'configure_opengraph_meta_tags' ] );
+
+		add_filter( 'awpcp-should-generate-rel-canonical', '__return_false' );
+		add_filter( 'awpcp-should-generate-basic-meta-tags', '__return_false' );
+		add_filter( 'awpcp-should-generate-title', '__return_false' );
+	}
+
+	/**
+	 * @since 4.1.0
+	 */
+	public function configure_canonical_url() {
+		add_filter( 'the_seo_framework_rel_canonical_output', [ $this, 'canonical_url' ] );
+	}
+
+	/**
+	 * @since 4.1.0
+	 */
+	public function configure_title_generation() {
+		add_filter( 'the_seo_framework_pre_get_document_title', [ $this, 'filter_document_title' ] );
+	}
+
+	/**
+	 * On Show Ad page:
+	 * - If the listing has a SEO override, we should use the override (don't forget
+	 * to replace any snippet variables included).
+	 * - If the listing has no SEO override, generate good default.
+	 *
+	 * On an Ad own page:
+	 * - If the listing has a SEO override, we use the override without attempting
+	 * to replace any variables. Yoast must have already done that.
+	 * - If the listing has no SEO override, generate a good default.
+	 *
+	 * @since 4.0.0
+	 */
+	public function configure_description_meta_tags() {
+		add_filter( 'the_seo_framework_description_output', [ $this, 'filter_listing_description' ] );
 	}
 
 	/**
@@ -96,8 +141,6 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function configure_opengraph_meta_tags() {
-		add_filter( 'the_seo_framework_pre_get_document_title', [ $this, 'filter_document_title' ] );
-		add_filter( 'the_seo_framework_description_output', [ $this, 'filter_listing_description' ] );
 		add_filter( 'the_seo_framework_ogtype_output', [ $this, 'filter_opengraph_type' ] );
 		add_filter( 'the_seo_framework_ogimage_output', [ $this, 'add_opengraph_images' ] );
 		add_filter( 'the_seo_framework_ogtitle_output', [ $this, 'filter_opengraph_title' ] );
@@ -107,7 +150,6 @@ class AWPCP_SEOFrameworkIntegration {
 		add_filter( 'the_seo_framework_twitterimage_output', [ $this, 'add_opengraph_images' ] );
 		add_filter( 'the_seo_framework_twitterdescription_output', [ $this, 'filter_twitter_description' ] );
 		add_filter( 'the_seo_framework_twittertitle_output', [ $this, 'filter_twitter_title' ] );
-		add_filter( 'the_seo_framework_rel_canonical_output', [ $this, 'canonical_url' ] );
 
 		return false;
 	}
@@ -117,7 +159,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_document_title( $title ) {
-		$override = get_post_meta( $this->current_listing->ID, '_genesis_title', true );
+		$override = $this->seo->get_custom_field( '_genesis_title', $this->current_listing->ID );
 
 		if ( empty( $override ) ) {
 			return $this->build_title( $title );
@@ -144,7 +186,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_listing_description( $description ) {
-		$override = get_post_meta( $this->current_listing->ID, '_genesis_description', true );
+		$override = $this->seo->get_custom_field( '_genesis_description', $this->current_listing->ID );
 
 		return $this->get_social_description( $description, $override );
 	}
@@ -182,7 +224,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function add_opengraph_images( $opengraph_image ) {
-		$override = get_post_meta( $this->current_listing->ID, '_social_image_url', true );
+		$override = $this->seo->get_custom_field( '_social_image_url', $this->current_listing->ID );
 
 		if ( empty( $override ) ) {
 			$featured_image = $this->attachments->get_featured_attachment_of_type(
@@ -205,7 +247,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_opengraph_title( $title ) {
-		$override = get_post_meta( $this->current_listing->ID, '_open_graph_title', true );
+		$override = $this->seo->get_custom_field( '_open_graph_title', $this->current_listing->ID );
 
 		return $this->get_social_title( $title, $override );
 	}
@@ -236,7 +278,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_opengraph_description( $description ) {
-		$override = get_post_meta( $this->current_listing->ID, '_open_graph_description', true );
+		$override = $this->seo->get_custom_field( '_open_graph_description', $this->current_listing->ID );
 
 		return $this->get_social_description( $description, $override );
 	}
@@ -245,7 +287,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_twitter_title( $title ) {
-		$override = get_post_meta( $this->current_listing->ID, '_twitter_title', true );
+		$override = $this->seo->get_custom_field( '_twitter_title', $this->current_listing->ID );
 
 		return $this->get_social_title( $title, $override );
 	}
@@ -254,7 +296,7 @@ class AWPCP_SEOFrameworkIntegration {
 	 * @since 4.1.0
 	 */
 	public function filter_twitter_description( $description ) {
-		$override = get_post_meta( $this->current_listing->ID, '_twitter_description', true );
+		$override = $this->seo->get_custom_field( '_twitter_description', $this->current_listing->ID );
 
 		return $this->get_social_description( $description, $override );
 	}
@@ -272,7 +314,12 @@ class AWPCP_SEOFrameworkIntegration {
 		return $url;
 	}
 
-	public function twitter_cards() {
+
+	/**
+	 * Needed to make twitter cards work On Show Ad page
+	 * @since 4.1.0
+	 */
+	public function twitter_cards( $retval ) {
 		return [ 'summary_large_image' ];
 	}
 }
