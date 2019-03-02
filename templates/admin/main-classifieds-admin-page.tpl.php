aee4f2ac1707afe1e051d3dff72c0e7e94f6c526
Willington Vega <wvega@wvega.com>
2019-03-02 13:27:52 -0500

    Use PHPCBF to fix PHPCS issues in main-classifieds-admin-page.tpl.php.
    There are several other issues that still need to be fixed.

--- templates/admin/main-classifieds-admin-page.tpl.php
+++ templates/admin/main-classifieds-admin-page.tpl.php
@@ -3,98 +3,114 @@
 <div class="awpcp-updated updated">
     <p>
         <?php $msg = _x( 'Thank you for using Another WordPress Classifieds Plugin, the #1 WordPress Classifieds Plugin.  Please direct support requests, enhancement ideas and bug reports to the %s.', '... to the <a>AWPCP Support Website link</a>', 'another-wordpress-classifieds-plugin' ); ?>
-        <?php echo sprintf($msg, '<a href="http://www.awpcp.com/forum/">' . __("AWPCP Support Website", "AWPCP") . '</a>'); ?>
+        <?php echo sprintf( $msg, '<a href="http://www.awpcp.com/forum/">' . __( 'AWPCP Support Website', 'AWPCP' ) . '</a>' ); ?>
     </p>
 </div>
 
-<?php if ($hasextrafieldsmodule == 1 && !($extrafieldsversioncompatibility == 1)): ?>
+<?php if ( $hasextrafieldsmodule == 1 && ! ( $extrafieldsversioncompatibility == 1 ) ) : ?>
 <div id="message" class="awpcp-updated updated fade">
     <p>
         <?php _e( 'The version of the extra fields module that you are using is not compatible with this version of Another WordPress Classifieds Plugin.', 'AWPCP' ); ?>
-        <a href="http://www.awpcp.com/contact/"><?php _e("Please request updated Extra Fields module files", "AWPCP"); ?></a>.
+        <a href="http://www.awpcp.com/contact/"><?php _e( 'Please request updated Extra Fields module files', 'AWPCP' ); ?></a>.
     </p>
 </div>
 <?php endif; ?>
 
 <div class="metabox-holder">
-    <div class="meta-box-sortables" <?php echo empty($sidebar) ? '' : ' style="float:left;width:70%;"'; ?>>
+    <div class="meta-box-sortables" <?php echo empty( $sidebar ) ? '' : ' style="float:left;width:70%;"'; ?>>
 
         <div class="postbox">
             <h3 class="hndle1"><span><?php _e( 'Another WordPress Classifieds Plugin Stats', 'AWPCP' ); ?><span></h3>
             <div class="inside">
                 <ul>
-                    <li><?php _e("AWPCP version", "AWPCP"); ?>: <strong><?php echo $awpcp_db_version; ?></strong>.</li>
+                    <li><?php _e( 'AWPCP version', 'AWPCP' ); ?>: <strong><?php echo $awpcp_db_version; ?></strong>.</li>
 
                     <?php $listings_collection = awpcp_listings_collection(); ?>
 
                     <?php $enabled_listings = $listings_collection->count_enabled_listings(); ?>
-                    <li><?php _e("Number of active listings currently in the system", "AWPCP"); ?>: <strong><?php echo $enabled_listings; ?></strong></li>
+                    <li><?php _e( 'Number of active listings currently in the system', 'AWPCP' ); ?>: <strong><?php echo $enabled_listings; ?></strong></li>
 
                     <?php $disabled_listings = $listings_collection->count_disabled_listings(); ?>
-                    <li><?php _e("Number of expired/disabled listings currently in the system", "AWPCP"); ?>: <strong><?php echo $disabled_listings; ?></strong></li>
+                    <li><?php _e( 'Number of expired/disabled listings currently in the system', 'AWPCP' ); ?>: <strong><?php echo $disabled_listings; ?></strong></li>
 
                     <?php $invalid_listings = $listings_collection->count_listings() - $enabled_listings - $disabled_listings; ?>
-                    <li><?php _e("Number of invalid listings currently in the system", "AWPCP"); ?>: <strong><?php echo $invalid_listings; ?></strong></li>
+                    <li><?php _e( 'Number of invalid listings currently in the system', 'AWPCP' ); ?>: <strong><?php echo $invalid_listings; ?></strong></li>
                 </ul>
 
                 <div style="border-top:1px solid #dddddd;">
-                <?php if (get_awpcp_option('freepay') == 1): ?>
-                    <?php if (adtermsset()): ?>
-                        <?php $msg = __("You have setup your listing fees. To edit your fees go to %s.", "AWPCP"); ?>
-                    <?php else: ?>
-                        <?php $msg = __("You have not configured your Listing fees. Go to %s to set up your listing fees. Once that is completed, if you are running in pay mode, the options will automatically appear on the listing form for users to fill out.", "AWPCP"); ?>
+                <?php if ( get_awpcp_option( 'freepay' ) == 1 ) : ?>
+                    <?php if ( adtermsset() ) : ?>
+                        <?php $msg = __( 'You have setup your listing fees. To edit your fees go to %s.', 'AWPCP' ); ?>
+                    <?php else : ?>
+                        <?php $msg = __( 'You have not configured your Listing fees. Go to %s to set up your listing fees. Once that is completed, if you are running in pay mode, the options will automatically appear on the listing form for users to fill out.', 'AWPCP' ); ?>
                     <?php endif; ?>
-                    <?php $url = add_query_arg('page', 'awpcp-admin-fees', admin_url('admin.php')); ?>
+                    <?php $url = add_query_arg( 'page', 'awpcp-admin-fees', admin_url( 'admin.php' ) ); ?>
                     <p><?php echo sprintf( $msg, sprintf( '<a href="%s">%s</a>', esc_url( $url ), __( 'Fees', 'another-wordpress-classifieds-plugin' ) ) ); ?></p>
-                <?php else: ?>
-                    <?php $msg = __("You currently have your system configured to run in free mode. To change to 'pay' mode go to %s and Check the box labeled 'Charge Listing Fee? (Pay Mode).'", "AWPCP"); ?>
-                    <?php $url = add_query_arg(array('page' => 'awpcp-admin-settings', 'g' => 'payment-settings'), admin_url('admin.php')); ?>
+                <?php else : ?>
+                    <?php $msg = __( "You currently have your system configured to run in free mode. To change to 'pay' mode go to %s and Check the box labeled 'Charge Listing Fee? (Pay Mode).'", 'AWPCP' ); ?>
+                    <?php
+                    $url = add_query_arg(
+                        [
+                            'page' => 'awpcp-admin-settings',
+                            'g'    => 'payment-settings',
+                        ],
+                        admin_url( 'admin.php' )
+                    );
+                    ?>
                     <p><?php echo sprintf( $msg, sprintf( '<a href="%s">%s</a>', esc_url( $url ), __( 'Payment Options', 'another-wordpress-classifieds-plugin' ) ) ); ?></p>
                 <?php endif; ?>
                 </div>
 
-                <?php if (categoriesexist()): ?>
+                <?php if ( categoriesexist() ) : ?>
 
                 <div style="border-top:1px solid #dddddd;">
-                    <?php $msg = __("Go to the %s section to edit/delete current categories or add new categories.", "AWPCP"); ?>
+                    <?php $msg = __( 'Go to the %s section to edit/delete current categories or add new categories.', 'AWPCP' ); ?>
                     <?php $url = awpcp_get_admin_categories_url(); ?>
-                    <p><?php echo sprintf($msg, sprintf('<a href="%s">%s</a>', $url, __('Manage Categories', 'another-wordpress-classifieds-plugin' ))); ?></p>
+                    <p><?php echo sprintf( $msg, sprintf( '<a href="%s">%s</a>', $url, __( 'Manage Categories', 'another-wordpress-classifieds-plugin' ) ) ); ?></p>
 
                     <ul>
                         <?php $totalcategories = countcategories(); ?>
                         <li style="margin-bottom:6px;list-style:none;">
-                            <?php _e("Total number of categories in the system", "AWPCP"); ?>:
+                            <?php _e( 'Total number of categories in the system', 'AWPCP' ); ?>:
                             <strong><?php echo $totalcategories; ?></strong>
                         </li>
 
                         <?php $totalparentcategories = countcategoriesparents(); ?>
                         <li style="margin-bottom:6px;list-style:none;">
-                            <?php _e("Number of Top Level parent categories", "AWPCP"); ?>:
+                            <?php _e( 'Number of Top Level parent categories', 'AWPCP' ); ?>:
                             <strong><?php echo $totalparentcategories; ?></strong>
                         </li>
 
                         <?php $totalchildrencategories = countcategorieschildren(); ?>
                         <li style="margin-bottom:6px;list-style:none;">
-                            <?php _e("Number of sub level children categories", "AWPCP"); ?>:
+                            <?php _e( 'Number of sub level children categories', 'AWPCP' ); ?>:
                             <strong><?php echo $totalchildrencategories; ?></strong>
                         </li>
                     </ul>
                 </div>
 
-                <?php else: ?>
+                <?php else : ?>
 
                 <div style="border-top:1px solid #dddddd;">
-                    <?php $msg = __("You have not categories defined. Go to the %s section to set up your categories.", "AWPCP"); ?>
-                    <?php $url = add_query_arg('page', 'Cofigure3', admin_url('admin.php')); ?>
+                    <?php $msg = __( 'You have not categories defined. Go to the %s section to set up your categories.', 'AWPCP' ); ?>
+                    <?php $url = add_query_arg( 'page', 'Cofigure3', admin_url( 'admin.php' ) ); ?>
                     <p><?php echo sprintf( $msg, sprintf( '<a href="%s">%s</a>', esc_url( $url ), __( 'Manage Categories', 'another-wordpress-classifieds-plugin' ) ) ); ?></p>
                 </div>
 
                 <?php endif; ?>
 
-                <?php if (get_awpcp_option('freepay') == 1): ?>
+                <?php if ( get_awpcp_option( 'freepay' ) == 1 ) : ?>
                 <div style="border-top:1px solid #dddddd;">
-                    <?php $msg = __("You currently have your system configured to run in pay mode. To change to 'free' mode go to %s and uncheck the box labeled 'Charge Listing Fee? (Pay Mode).'", "AWPCP"); ?>
-                    <?php $url = add_query_arg(array('page' => 'awpcp-admin-settings', 'g' => 'payment-settings'), admin_url('admin.php')); ?>
+                    <?php $msg = __( "You currently have your system configured to run in pay mode. To change to 'free' mode go to %s and uncheck the box labeled 'Charge Listing Fee? (Pay Mode).'", 'AWPCP' ); ?>
+                    <?php
+                    $url = add_query_arg(
+                        [
+                            'page' => 'awpcp-admin-settings',
+                            'g'    => 'payment-settings',
+                        ],
+                        admin_url( 'admin.php' )
+                    );
+                    ?>
                     <p><?php echo sprintf( $msg, sprintf( '<a href="%s">%s</a>', esc_url( $url ), __( 'Payment Options', 'another-wordpress-classifieds-plugin' ) ) ); ?></p>
                 </div>
                 <?php endif; ?>
