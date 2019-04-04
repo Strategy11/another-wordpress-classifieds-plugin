cc3f8980b57c90def492548a9de0e551f0620acd
Juan <juan@freerange.com>
2019-04-04 23:17:28 +0200

    change button position

--- includes/settings/renderers/class-button-settings-renderer.php
+++ includes/settings/renderers/class-button-settings-renderer.php
@@ -38,8 +38,8 @@ class AWPCP_ButtonSettingsRenderer {
         $html .= '<div id="pop-' . esc_attr( $setting['id'] ) . '" style="display:none">';
         $html .= '<h2 style="text-align: center">' . __( 'Resetting to the default layout will cause any custom HTML layout changes you\'ve made to be lost. Are you sure?', 'another-wordpress-classifieds-plugin' ) . '</h2>';
         $html .= '<p style="text-align: center">';
-        $html .= '<button data-nonce="' . esc_attr( $nonce ) . '" id="' . esc_attr( $setting['id'] ) . '" class="button-primary">' . __( 'Reset', ' another-wordpress-classifieds-plugin' ) . '</button> ';
-        $html .= '<button  class="button-secondary TB_closeWindowButton">' . __( 'Cancel', ' another-wordpress-classifieds-plugin' ) . '</button></p>';
+        $html .= '<button  class="button-secondary TB_closeWindowButton">' . __( 'Cancel', ' another-wordpress-classifieds-plugin' ) . '</button> ';
+        $html .= '<button data-nonce="' . esc_attr( $nonce ) . '" id="' . esc_attr( $setting['id'] ) . '" class="button-primary">' . __( 'Reset', ' another-wordpress-classifieds-plugin' ) . '</button></p>';
         $html .= '</div>';
         // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
         echo $html;
