7e333750be26ad2196dbb375cf9190a975bf60de
Willington Vega <wvega@wvega.com>
2019-04-26 12:11:12 -0500

    Fix PHPCS issue in class-upgrade-container-configuration.php.

--- includes/upgrade/class-upgrade-container-configuration.php
+++ includes/upgrade/class-upgrade-container-configuration.php
@@ -8,6 +8,8 @@
 /**
  * Container configuration object responsible for registering classes used
  * in manual upgrade routines.
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class AWPCP_UpgradeContainerConfiguration implements AWPCP_ContainerConfigurationInterface {
 
