3a8e6d47725df646e6f106bdea865c0fb41c3728
Willington Vega <wvega@wvega.com>
2019-06-19 15:41:45 -0500

    Remove unused variables.

--- includes/class-listings-collection.php
+++ includes/class-listings-collection.php
@@ -521,9 +521,6 @@ class AWPCP_ListingsCollection {
             $query['order'] = 'DESC';
         }
 
-        $basedate = 'CASE WHEN renewed_date IS NULL THEN ad_startdate ELSE GREATEST(ad_startdate, renewed_date) END';
-        $is_paid = 'CASE WHEN ad_fee_paid > 0 THEN 1 ELSE 0 END';
-
         switch ( $orderby ) {
             case 1:
                 $query['meta_key'] = '_awpcp_most_recent_start_date';
