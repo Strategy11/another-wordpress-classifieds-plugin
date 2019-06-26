8ab65614015378be32754e58e2534a4ce8e9741f
Juan <juan@admin123.net>
2019-06-26 08:45:10 -0400

    phpcs

--- includes/listings/class-listings-view-counter.php
+++ includes/listings/class-listings-view-counter.php
@@ -1,23 +1,29 @@
 <?php
+/**
+ * @package AWPCP\Listings
+ */
 
+/**
+ * Increments the ad view counter.
+ */
 class AWPCP_ListingsViewCounter extends AWPCP_AjaxHandler {
 
     private $response;
     private $request;
-    private $listingsLogic;
+    private $listings_logic;
 
-    public function __construct( $response, $request, $listingsLogic ) {
+    public function __construct( $response, $request, $listings_login ) {
         parent::__construct( $response );
-        $this->request = $request;
-        $this->response = $response;
-        $this->listingsLogic = $listingsLogic;
+        $this->request        = $request;
+        $this->response       = $response;
+        $this->listings_logic = $listings_login;
     }
 
     public function ajax() {
         if ( ! $this->request->is_bot() ) {
-            $listing_id = $this->request->post('listing_id');
-            $listing = get_post($listing_id);
-            $this->listingsLogic->increase_visits_count( $listing );
+            $listing_id = $this->request->post( 'listing_id' );
+            $listing    = get_post( $listing_id );
+            $this->listings_logic->increase_visits_count( $listing );
         }
     }
 }
