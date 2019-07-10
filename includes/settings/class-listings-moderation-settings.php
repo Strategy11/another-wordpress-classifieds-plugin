2ad8e2fe804d269701ae835dd0b16ef0f35916d6
Willington Vega <wvega@wvega.com>
2019-07-10 14:27:39 -0500

    Fix PHPMD issues in class-listings-moderation-settings.php.

--- includes/settings/class-listings-moderation-settings.php
+++ includes/settings/class-listings-moderation-settings.php
@@ -15,6 +15,9 @@ class AWPCP_ListingsModerationSettings {
         $this->settings = $settings;
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function validate_all_settings( $options, $group ) {
         if ( isset( $options['requireuserregistration'] ) && $options['requireuserregistration'] && $this->settings->get_option( 'enable-email-verification' ) ) {
             $message = __( "Email verification was disabled because you enabled Require Registration. Registered users don't need to verify the email address used for contact information.", 'another-wordpress-classifieds-plugin' );
@@ -26,6 +29,9 @@ class AWPCP_ListingsModerationSettings {
         return $options;
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function validate_group_settings( $options, $group ) {
         if ( isset( $options['enable-email-verification'] ) && $options['enable-email-verification'] && $this->settings->get_option( 'requireuserregistration' ) ) {
             $message = __( "Email verification was not enabled because Require Registration is on. Registered users don't need to verify the email address used for contact information.", 'another-wordpress-classifieds-plugin' );
