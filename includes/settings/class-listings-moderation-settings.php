0ee1e651e81dce7bd29341d8fe6fbb1bda47dd68
Willington Vega <wvega@wvega.com>
2019-07-10 14:35:15 -0500

    Stop using get_awpcp_option() in class-listings-moderation-settings.php.

--- includes/settings/class-listings-moderation-settings.php
+++ includes/settings/class-listings-moderation-settings.php
@@ -16,7 +16,7 @@ class AWPCP_ListingsModerationSettings {
     }
 
     public function validate_all_settings( $options, $group ) {
-        if ( isset( $options['requireuserregistration'] ) && $options['requireuserregistration'] && get_awpcp_option( 'enable-email-verification' ) ) {
+        if ( isset( $options['requireuserregistration'] ) && $options['requireuserregistration'] && $this->settings->get_option( 'enable-email-verification' ) ) {
             $message = __( "Email verification was disabled because you enabled Require Registration. Registered users don't need to verify the email address used for contact information.", 'another-wordpress-classifieds-plugin' );
             awpcp_flash_warning( $message );
 
@@ -27,7 +27,7 @@ class AWPCP_ListingsModerationSettings {
     }
 
     public function validate_group_settings( $options, $group ) {
-        if ( isset( $options['enable-email-verification'] ) && $options['enable-email-verification'] && get_awpcp_option( 'requireuserregistration' ) ) {
+        if ( isset( $options['enable-email-verification'] ) && $options['enable-email-verification'] && $this->settings->get_option( 'requireuserregistration' ) ) {
             $message = __( "Email verification was not enabled because Require Registration is on. Registered users don't need to verify the email address used for contact information.", 'another-wordpress-classifieds-plugin' );
             awpcp_flash_warning( $message );
 
