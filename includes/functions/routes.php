13d540c168d8d6ed8eab931ec9ebabca28f2ed3e
Willington Vega <wvega@wvega.com>
2019-07-23 09:35:21 -0500

    Fix Undefined variable PHP notice.

--- includes/functions/routes.php
+++ includes/functions/routes.php
@@ -532,6 +532,8 @@ function awpcp_get_renew_ad_url($ad_id) {
     $hash = awpcp_get_renew_ad_hash( $ad_id );
 
     if ( get_awpcp_option( 'enable-user-panel' ) == 1 ) {
+        $url = awpcp_get_user_panel_url();
+
         $params = [
             'id'          => $ad_id,
             'page'        => 'awpcp-admin-renew-listing',
