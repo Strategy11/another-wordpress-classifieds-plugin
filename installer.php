f6fe07daaa412d11c0fff5725f4920baa38cd4d0
Willington Vega <wvega@wvega.com>
2019-07-29 20:13:49 -0500

    Update ad_regions table records that have no old_listing_id only.
    This prevents overwritting the original old_listing_id value if the user is
    running the upgrade routines a second time.

--- installer.php
+++ installer.php
@@ -818,7 +818,7 @@ class AWPCP_Installer {
             $wpdb->query( 'ALTER TABLE ' . AWPCP_TABLE_AD_REGIONS . ' ADD `old_listing_id` INT(10) NOT NULL AFTER `ad_id`' );
         }
 
-        $wpdb->query( 'UPDATE ' . AWPCP_TABLE_AD_REGIONS . ' SET `old_listing_id` = `ad_id`' );
+        $wpdb->query( 'UPDATE ' . AWPCP_TABLE_AD_REGIONS . ' SET `old_listing_id` = `ad_id` WHERE old_listing_id = 0' );
     }
 
     private function migrate_wordpress_page_settings() {
