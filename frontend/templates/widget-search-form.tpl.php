de750509f45bb891246fa0e9bb3ac3581df4944e
Willington Vega <wvega@wvega.com>
2019-08-13 11:06:55 -0500

    Prevent Undefined index PHP Notice in widget-search.php.

--- frontend/templates/widget-search-form.tpl.php
+++ frontend/templates/widget-search-form.tpl.php
@@ -9,24 +9,31 @@
 </p>
 
 <p>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_keyword' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_keyword' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_keyword' ) ); ?>" type="checkbox" value="1" <?php echo $show_keyword == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_keyword' ) ); ?>"><?php _e('Show keyword field?', 'another-wordpress-classifieds-plugin'); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_by' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_by' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_by' ) ); ?>" type="checkbox" value="1" <?php echo $show_by == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_by' ) ); ?>"><?php _e('Show Posted By field?', 'another-wordpress-classifieds-plugin'); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_city' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_city' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_city' ) ); ?>" type="checkbox" value="1" <?php echo $show_city == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_city' ) ); ?>"><?php _e('Show City field?', 'another-wordpress-classifieds-plugin'); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_county' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_county' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_county' ) ); ?>" type="checkbox" value="1" <?php checked( $show_county, 1 ); ?>/>
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_county' ) ); ?>"><?php esc_html_e( 'Show County field?', 'another-wordpress-classifieds-plugin' ); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_state' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_state' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_state' ) ); ?>" type="checkbox" value="1" <?php echo $show_state == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_state' ) ); ?>"><?php _e('Show State field?', 'another-wordpress-classifieds-plugin'); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_country' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_country' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_country' ) ); ?>" type="checkbox" value="1" <?php echo $show_country == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_country' ) ); ?>"><?php _e('Show Country field?', 'another-wordpress-classifieds-plugin'); ?></label>
     <br/>
+    <input name="<?php echo esc_attr( $this->get_field_name( 'show_category' ) ); ?>" type="hidden" value="0" />
     <input id="<?php echo esc_attr( $this->get_field_id( 'show_category' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_category' ) ); ?>" type="checkbox" value="1" <?php echo $show_category == 1 ? " checked='checked'" : "" ?> />
     <label for="<?php echo esc_attr( $this->get_field_id( 'show_category' ) ); ?>"><?php _e('Show Category field?', 'another-wordpress-classifieds-plugin'); ?></label>
 </p>
