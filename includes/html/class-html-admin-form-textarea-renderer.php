19766036532c92a683b866e5c364424645d470bc
Willington Vega <wvega@wvega.com>
2019-08-20 18:18:49 -0500

    Fix PHPCS issues in class-html-admin-form-textarea-renderer.php.

--- includes/html/class-html-admin-form-textarea-renderer.php
+++ includes/html/class-html-admin-form-textarea-renderer.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * @package AWPCP\HTML
+ */
 
+/**
+ * Constructor function for HTML Admin Form Textarea Renderer class.
+ */
 function awpcp_html_admin_form_textarea_renderer() {
     return new AWPCP_HTML_Admin_Form_Textarea_Renderer();
 }
@@ -18,32 +24,32 @@ class AWPCP_HTML_Admin_Form_Textarea_Renderer implements AWPCP_HTML_Element_Rend
         $form_field_id = "awpcp-admin-form-textarea-{$element_definition['#name']}";
 
         $form_field_definition = array(
-            '#type' => 'div',
+            '#type'       => 'div',
             '#attributes' => $this->get_form_field_attributes( $element_definition ),
-            '#content' => array(
+            '#content'    => array(
                 array(
-                    '#type' => 'label',
+                    '#type'       => 'label',
                     '#attributes' => array( 'for' => $form_field_id ),
-                    '#content' => $element_definition['#label'],
+                    '#content'    => $element_definition['#label'],
                 ),
                 array(
-                    '#type' => 'textarea',
+                    '#type'       => 'textarea',
                     '#attributes' => array(
-                        'id' => $form_field_id,
+                        'id'   => $form_field_id,
                         'name' => $element_definition['#name'],
                         'cols' => $element_definition['#cols'],
                         'rows' => $element_definition['#rows'],
                     ),
-                    '#content' => esc_html( $element_definition['#value'] )
-                )
-            )
+                    '#content'    => esc_html( $element_definition['#value'] ),
+                ),
+            ),
         );
 
         return $html_renderer->render_element( $form_field_definition );
     }
 
     private function get_form_field_attributes( $element_definition ) {
-        $form_field_attributes = awpcp_parse_html_attributes( $element_definition['#attributes'] );
+        $form_field_attributes            = awpcp_parse_html_attributes( $element_definition['#attributes'] );
         $form_field_attributes['class'][] = 'awpcp-admin-form-textarea';
 
         return $form_field_attributes;
