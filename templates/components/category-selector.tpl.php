e7059e4b64a065d587be7589b84a2394eca5ca73
Juan <juandlopezr@gmail.com>
2019-10-02 10:37:45 -0400

    fix selectbox overflowing container

--- templates/components/category-selector.tpl.php
+++ templates/components/category-selector.tpl.php
@@ -7,7 +7,7 @@
 <label class="awpcp-category-dropdown-label" for="awpcp-category-dropdown-<?php echo esc_attr( $hash ); ?>"><?php echo esc_html( $label ); ?><?php echo $required ? '<span class="required">*</span>' : ''; // XSS Ok. ?></label>
 <?php endif; ?>
 
-<select id="awpcp-category-dropdown-<?php echo esc_attr( $hash ); ?>" class="awpcp-category-dropdown awpcp-dropdown <?php echo $required ? 'required' : ''; ?>" name="<?php echo esc_attr( $name ); ?>" data-hash="<?php echo esc_attr( $hash ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>"<?php echo $multiple ? ' multiple="multiple"' : ''; ?><?php echo $auto ? ' data-auto="auto"' : ''; ?>>
+<select id="awpcp-category-dropdown-<?php echo esc_attr( $hash ); ?>" class="awpcp-category-dropdown awpcp-dropdown <?php echo $required ? 'required' : ''; ?>" name="<?php echo esc_attr( $name ); ?>" data-hash="<?php echo esc_attr( $hash ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>"<?php echo $multiple ? ' multiple="multiple"' : ''; ?><?php echo $auto ? ' data-auto="auto"' : ''; ?> style="width: 100%">
     <?php if ( ! $multiple ) : ?>
     <option class="awpcp-dropdown-placeholder"><?php echo esc_html( $placeholder ); ?></option>
     <?php endif; ?>
