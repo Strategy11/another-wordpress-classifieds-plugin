a5d89efd0b648b8e70144d676b6651f36ae4b77a
Diego Giraldo <diego.dgiraldo36@gmail.com>
2020-06-25 13:52:58 -0500

    Fix latests ads widget CSS classes. Related #2991

--- frontend/widget-latest-ads.php
+++ frontend/widget-latest-ads.php
@@ -126,10 +126,10 @@ class AWPCP_LatestAdsWidget extends WP_Widget {
         $html_image = $this->render_item_image( $item, $instance );
 
         if ( ! empty( $html_image ) ) {
-            $html_class .= ' awpcp-listings-widget-item-without-thumbnail';
-
             $template = '<li class="awpcp-listings-widget-item %1$s"><div class="awpcplatestbox awpcp-clearfix"><div class="awpcplatestthumb awpcp-clearfix">%2$s</div><div class="awpcp-listings-widget-item--title-and-content">%3$s %4$s</div>%5$s</div></li>';
         } else {
+            $html_class .= ' awpcp-listings-widget-item-without-thumbnail';
+            
             $template = '<li class="awpcp-listings-widget-item %1$s"><div class="awpcplatestbox awpcp-clearfix"><div class="awpcp-listings-widget-item--title-and-content">%3$s %4$s</div>%5$s</div></li>';
         }
 
