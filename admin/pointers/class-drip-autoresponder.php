349d13aeb80f15fdabe32ac71aa10a804bbde1b8
Steph <steph@strategy11.com>
2020-08-21 08:12:46 -0600

    missed a free module mention

--- admin/pointers/class-drip-autoresponder.php
+++ admin/pointers/class-drip-autoresponder.php
@@ -62,7 +62,7 @@ class AWPCP_DripAutoresponder {
         $template = '<h3><title></h3><p><content></p><p><label for="awpcp-autoresponder-email"><b><label>:</b></label><br><input style="min-width: 100%" id="awpcp-autoresponder-email" type="text" name="awpcp-user-email" value="<user-email>" /></p><div class="awpcp-message error is-hidden"></div>';
 
         $title = _x( 'Want to know the Secrets of Building an Awesome Classifieds Website?', 'drip-autoresponder', 'another-wordpress-classifieds-plugin' );
-        $content = _x( 'Find out how to create a compelling, thriving classifieds site from scratch in this ridiculously actionable (and free) 5-part email course. Get a premium module (AWPCP XML Sitemap) for free just for signing up.', 'drip-autoresponder', 'another-wordpress-classifieds-plugin' );
+        $content = _x( 'Find out how to create a compelling, thriving classifieds site from scratch in this ridiculously actionable (and free) 5-part email course.', 'drip-autoresponder', 'another-wordpress-classifieds-plugin' );
 
         $template = str_replace( '<title>', $title, $template );
         $template = str_replace( '<content>', $content, $template );
