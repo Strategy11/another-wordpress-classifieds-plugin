bc4d055f508a5ba808d0289118a7c42a258b19f7
Steph <steph@strategy11.com>
2020-10-29 10:45:39 -0600

    revert last commit

--- includes/models/class-custom-post-types.php
+++ includes/models/class-custom-post-types.php
@@ -97,7 +97,7 @@ class AWPCP_Custom_Post_Types {
                 'publicly_queryable'   => true,
                 'show_ui'              => true,
                 'show_in_nav_menus'    => true,
-                'show_in_menu'         => true,
+                'show_in_menu'         => false,
                 'show_in_admin_bar'    => true,
                 'menu_icon'            => null,
                 'menu_position'        => 26,
