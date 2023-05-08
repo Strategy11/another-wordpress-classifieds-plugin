e8c22bef77f1aa6348ad9a799a72f37a886f64c9
mostafa <mostafa.hisham.mahmoud@gmail.com>
2023-05-08 22:06:43 +0200

    find patchwork

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,8 +18,8 @@ define( 'AWPCP_DIR', dirname( __DIR__ ) );
 define( 'AWPCP_URL', 'https://example.org/wp-content/plugins/another-wordpress-classifieds-plugin' );
 
 $patchwork = AWPCP_DIR . '/vendor/antecedent/patchwork/Patchwork.php';
-if ( getenv( 'IS_GITHB_ACTION' ) ) {
-    $patchwork = '~/.composer/vendor/antecedent/patchwork/Patchwork.php';
+if ( getenv( 'RUNNER_COMPOSER_PATH' ) ) {
+    $patchwork = getenv( 'RUNNER_COMPOSER_PATH' ).'vendor/antecedent/patchwork/Patchwork.php';
 }
 echo $patchwork .PHP_EOL . PHP_EOL;
 if ( file_exists( $patchwork ) ) {
