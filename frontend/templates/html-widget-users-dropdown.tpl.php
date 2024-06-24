aed060ad52e3b64c0da4b8855bdb92de7676e63a
stephywells <steph@strategy11.com>
2024-06-24 17:37:48 -0600

    Remove added whitespace

--- frontend/templates/html-widget-users-dropdown.tpl.php
+++ frontend/templates/html-widget-users-dropdown.tpl.php
@@ -11,9 +11,11 @@
     <option value=""><?php echo esc_html( $args['default'] ); ?></option>
     <?php endif; ?>
     <?php foreach ( $args['users'] as $k => $user ): ?>
-    <option value="<?php echo esc_attr( $user->ID ); ?>"
-        <?php if ( $args['include-full-user-information'] ): ?> data-user-information="<?php echo esc_attr( wp_json_encode( $user ) ); ?>"<?php endif; ?>
-        <?php echo $args['selected'] == $user->ID ? 'selected="selected"' : ''; ?>>
+    <option value="<?php echo esc_attr( $user->ID ); ?>" <?php
+        if ( $args['include-full-user-information'] ):
+            ?>data-user-information="<?php echo esc_attr( wp_json_encode( $user ) ); ?>" <?php
+        endif;
+        echo $args['selected'] == $user->ID ? 'selected="selected"' : ''; ?>>
         <?php echo esc_html( $user->public_name ); ?>
     </option>
     <?php endforeach; ?>
