3718d9ee109ed963b39cc61ab47290f524bb8d9c
Sherv <sherv.elmi@gmail.com>
2024-11-19 13:27:35 +0300

    Finish success-step template content

--- templates/admin/onboarding-wizard/steps/success-step.tpl.php
+++ templates/admin/onboarding-wizard/steps/success-step.tpl.php
@@ -2,7 +2,7 @@
 /**
  * Onboarding Wizard - Success (You're All Set!) Step.
  *
- * @package Tasty_Recipes
+ * @package AWPCP\Admin
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,23 +12,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <section id="awpcp-onboarding-success-step" class="awpcp-onboarding-step awpcp-card-box hidden" data-step-name="<?php echo esc_attr( $step ); ?>">
     <div class="awpcp-card-box-header">
-        <?php // awpcp_inline_svg( 'icon-check.svg' ); ?>
+        <?php awpcp_inline_svg( 'logo.svg' ); ?>
     </div>
 
     <div class="awpcp-card-box-content">
         <h2 class="awpcp-card-box-title"><?php esc_html_e( 'You\'re All Set!', 'another-wordpress-classifieds-plugin' ); ?></h2>
         <p class="awpcp-card-box-text">
-            <?php esc_html_e( 'Congratulations on completing the onboarding process! We hope you enjoy using Tasty Recipes Lite Plugin.', 'another-wordpress-classifieds-plugin' ); ?>
+            <?php esc_html_e( 'Congratulations on completing the onboarding process! We hope you enjoy using AWP Classifieds Plugin.', 'another-wordpress-classifieds-plugin' ); ?>
         </p>
     </div>
 
     <div class="awpcp-card-box-footer">
-        <a href="<?php echo '#'; ?>" class="tasty-button tasty-highlight">
-            <?php esc_html_e( 'Go to Dashboard', 'another-wordpress-classifieds-plugin' ); ?>
+        <a href="<?php echo esc_url( admin_url( 'edit.php?post_type=awpcp_listing' ) ); ?>" class="button button-secondary">
+            <?php esc_html_e( 'Create a Classified Ad', 'another-wordpress-classifieds-plugin' ); ?>
         </a>
 
-        <a href="<?php echo '#'; ?>" class="tasty-button tasty-button-pink">
-            <?php esc_html_e( 'Go to Settings', 'another-wordpress-classifieds-plugin' ); ?>
+        <a href="<?php echo esc_url( admin_url( 'admin.php?page=awpcp.php' ) ); ?>" class="button button-primary">
+            <?php esc_html_e( 'Go to Dashboard', 'another-wordpress-classifieds-plugin' ); ?>
         </a>
     </div>
 </section>
