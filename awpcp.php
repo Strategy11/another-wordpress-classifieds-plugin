41f76d8506851782664c715b0d3c69d82c2bbe4f
Sorin Marta <sorinmarta0@gmail.com>
2024-12-05 16:21:48 +0200

    Move load_text_domain to the init hook

--- awpcp.php
+++ awpcp.php
@@ -101,6 +101,11 @@ if ( file_exists( AWPCP_DIR . '/awpcp_category_icons_module.php' ) ) {
 }
 /* End of legacy code. */
 
+// Load text domain in init to match the changes in WP 6.7.
+add_action( 'init', function () {
+    awpcp_load_plugin_textdomain( AWPCP_FILE );
+} );
+
 /**
  * BuddyPress normally attaches bp_loaded to plugins_loaded with priority 10.
  * When changing the priorities below, please make sure that modules are
