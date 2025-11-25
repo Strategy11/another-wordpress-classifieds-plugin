dca9ff85c18d130445ff3996c7801c9e101565aa
Sorin Marta <sorinmarta0@gmail.com>
2025-11-25 10:19:16 +0200

    phpcs fix

--- includes/listings/class-listings-content.php
+++ includes/listings/class-listings-content.php
@@ -74,7 +74,7 @@ class AWPCP_ListingsContent {
         /**
          * Allow disabling the rendering of shortcodes in listings content.
          * We run it after the single listing check to avoid unnecessary callbacks.
-         * 
+         *
          * @since x.x
          */
         if ( apply_filters( 'awpcp_disable_listing_shortcode_stripping', false ) ) {
